8f529335d9f69d3b7f78f0a37c30f35afd4fe207
mixasik03 <o.mixasik2003@yandex.ru>
2021-09-29 14:53:06 +0300

    Задание 4

---
+++ СР 1/СР 1/Заданте 4/Заданте 4/Заданте 4.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	double vis, sh, l, vis1, sh1;
+	cout << "vis=  ";   cin >> vis;
+	cout << "sh=  ";   cin >> sh;
+	cout << "l=  ";   cin >> l;
+	cout << "vis1=  ";   cin >> vis1;
+	cout << "sh1=  ";   cin >> sh1;
+	if (vis > 0 && sh > 0 && l > 0 && vis1 > 0 && sh1 > 0);
+	{
+		if (vis <= vis1 && sh <= sh1) cout << "ohhh myyy. NICE";
+		else  cout << "ERRORRRRR";
+	}
+}
