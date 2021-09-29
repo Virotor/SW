aaf7e51d6b54ca929fb0ab4b68529112247b1a8d
AndreiZastupnevich <andreizastupnevich@gmail.com>
2021-09-29 22:18:52 +0300

    1
    type (scope) : short summary
    
    type : init
    
    scope : T1
    
    short summary :  solution of the task

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int num, size;
+	char ch;
+	string str;
+	cout << "Введите целое число" << endl;
+	cin >> num;
+	while (num > 0)
+	{
+		ch = num % 10 + 48;
+		str += ch;
+		num /= 10;
+	}
+	size = str.size();
+	for (int i = size - 1; i >= 0; i--)
+	{
+		cout << str[i] << endl;
+	}
+	return 0;
+}
