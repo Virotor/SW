fbc20e04b48136d512a6dc83000b7078f036ea4a
CrypeDJ <litowrog@gmail.com>
2021-09-29 20:25:20 +0300

    init(T1):task 1

---
+++ SR1 progect 1/SR1 progect 1/SR1 progect 1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while (n > 9) {
+        cout << n % 10 << endl;
+        n /= 10;
+    }
+    cout << n;
+    return 0;
+}
