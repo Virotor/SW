91bc47458502bc0d93ca9f1e3b11a050ba0bf14b
Koffdev <90766635+Koffdev@users.noreply.github.com>
2021-09-29 22:32:50 +0300

    init (task1) : add task 1

---
+++ Project_Tasks/trpo1/trpo1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int tseloeNum;
+	cout << "Введите число: \n";
+	cin >> tseloeNum;
+	int delitelNum = 1;
+	while ((tseloeNum / delitelNum) >= 1)
+	{
+		delitelNum *= 10;
+	}
+	delitelNum /= 10;
+	while (delitelNum >= 1)
+	{
+		cout << (tseloeNum / delitelNum) % 10 << endl;
+		delitelNum /= 10;
+	}
+	return 0;
+}
