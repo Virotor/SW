a914cda8e5ebf43d2782e63f6d33e73848ec9b41
Shveder <89966688+Shveder@users.noreply.github.com>
2021-09-29 17:04:37 +0300

    sw задание 3
    Почему то не работает с большими числами

---
+++ sw ex 3/sw ex 3/Source.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	printf("Input n and k:\n");
+	int n, k;
+	scanf_s("%d%d", &n, &k);
+	int kolichestvoCifr;
+	int byfer;
+	byfer = n;
+	kolichestvoCifr = 0;
+	for (int i = 0;n > 0; i++) 
+	{
+		n = n / 10;
+		kolichestvoCifr += 1;
+	}
+	int novoeChislo, vspomogatelnoeChislo;
+	vspomogatelnoeChislo = pow(10, kolichestvoCifr - k);
+	novoeChislo = byfer/vspomogatelnoeChislo;
+
+	printf("Rezult:%d",novoeChislo);
+	return 0;
+}
