90619a6d70e888b7bae8a5992a97e6b91b3c1662
Lightning-Andrey <andrejkamors@gmail.com>
2021-09-29 19:45:58 +0300

    feat(Task#1):feat'n'fix
    Добавил задание 4 и навёл красоту в предыдущих

--- ТРПО СР №1/Source.cpp
+++ ТРПО СР №1/Source.cpp
@@ -15,10 +15,10 @@ int main()
 	} while ( number<= 0  or trunc(number) < number);
 
 	while (factor < number/10) factor *= 10;
-
+	cout << endl;
 	while (factor >= 1)
 	{
-		cout<< "\n\t" << (int)(number / factor) << endl;
+		cout<< "\t" << (int)(number / factor) << endl;
 		number -= (int)(number / factor)*factor;
 		factor /= 10;
 	}
@@ -50,16 +50,144 @@ int main()
 		cout << "\tk = ";
 		cin >> k;
 	} while (n <= 0 or trunc(n) < n or k <= 0 or trunc(k) < k);
+	
 	quotient = n;
 	count -= k;
-	cout << count<<endl;
 	while (quotient > 10)
 	{
 		count++;
 		quotient /= 10;
+	}	
+	if (count <= 0)
+		cout << "\n\tIt's simple: " << n << endl;
+	else
+		cout << "\n\tFirst " << k << " digits of " << n << " : " << (int)(n / pow(10, count)) << endl;
+	
+
+	cout << "\n-----------PROGRAM#4-----------------------------------------------------------------\n";
+
+	double size[6], length, height, width, hlength, hheight, hwidth;
+
+	do
+	{
+		cout << "\n\tEnter positive length, height and width of hole and parallelepiped.\n\n\tParallelepiped:" << endl;
+		cout << "\tlength = ";
+		cin >> length;
+		cout << "\theight = ";
+		cin >> height;
+		cout << "\twidth = ";
+		cin >> width;
+		cout << "\n\tHole:\n\tlength = ";
+		cin >> hlength;
+		cout << "\tdepth = ";
+		cin >> hheight;
+		cout << "\twidth = ";
+		cin >> hwidth;
+	} while (length <= 0 or height <= 0 or width <= 0 or hlength <= 0 or hheight <= 0 or hwidth <= 0);
+
+	if (length > height)
+	{
+		size[2] = length;
+		if (width > length)
+		{
+			size[2] = width;
+			size[1] = length;
+			size[0] = height;
+		}
+		else
+			if (width > height)
+			{
+				size[1] = width;
+				size[0] = height;
+			}
+			else
+			{
+				size[1] = height;
+				size[0] = width;
+			}
 	}
-	cout << count<<endl;
-	cout<<"\n\tFirst "<<k<<" digits of "<<n<<" : " << (int)(n / pow(10, count))<<endl;
+	else
+	{
+		size[2] = height;
+		if (width > height)
+		{
+			size[2] = width;
+			size[1] = height;
+			size[0] = length;
+		}
+		else
+			if (width > length)
+			{
+				size[1] = width;
+				size[0] = length;
+			}
+			else
+			{
+				size[1] = length;
+				size[0] = width;
+			}
+	}
+	
+	if (hlength > hheight)
+	{
+		size[5] = hlength;
+		if (hwidth > hlength)
+		{
+			size[5] = hwidth;
+			size[4] = hlength;
+			size[3] = hheight;
+		}
+		else
+			if (hwidth > hheight)
+			{
+				size[4] = hwidth;
+				size[3] = hheight;
+			}
+			else
+			{
+				size[4] = hheight;
+				size[3] = hwidth;
+			}
+	}
+	else
+	{
+		size[5] = hheight;
+		if (hwidth > hheight)
+		{
+			size[5] = hwidth;
+			size[4] = hheight;
+			size[3] = hlength;
+		}
+		else
+			if (hwidth > hlength)
+			{
+				size[4] = hwidth;
+				size[3] = hlength;
+			}
+			else
+			{
+				size[4] = hlength;
+				size[3] = hwidth;
+			}
+	}
+	//for (int i = 0; i <= 5; i++)cout << size[i];
+	for (int i = 0; i <= 2; i++)
+	{
+		if (size[i] <= size[i + 3])
+			if (i <= 1)
+				continue;
+			else
+			{
+				cout << "\n\tToo little parallelepiped!\n\tOn vlazit!!!" << endl;
+				break;
+			}
+		else
+		{
+			cout << "\n\tToo little hole!\n\tOn ne vlazit!!!"<<endl;
+			break;
+		}
+	}
+	
 
 	return 0;
 }
