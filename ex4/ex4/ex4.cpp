2815744bd4b445cef01ae8866d0eb2c5e4f98f05
Arina <melkumova.arina@gmail.com>
2021-09-29 23:00:44 +0300

    feat(T4): add 'figure and hole' programm

---
+++ ex4/ex4/ex4.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<cmath>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int holeLength, holeWidth; //4 задание
+
+	cout << "Enter hole length: "; cin >> holeLength;
+	cout << "Enter hole width: "; cin >> holeWidth;
+	int figureLength, figureWidth, figureHeight;
+	cout << "Enter figure length: "; cin >> figureLength;
+	cout << "Enter figure width: "; cin >> figureWidth;
+	cout << "Enter figure height: "; cin >> figureHeight;
+	if (((holeLength >= figureLength && holeWidth >= figureWidth) || (holeLength >= figureWidth && holeWidth >= figureLength)) ||
+		((holeLength >= figureLength && holeWidth >= figureHeight) || (holeLength >= figureHeight && holeWidth >= figureLength)) ||
+		((holeLength >= figureHeight && holeWidth >= figureWidth) || (holeLength >= figureWidth && holeWidth >= figureHeight))) {
+		cout << "Parallelepiped will pass into the hole!" << endl;
+	}
+	else {
+		cout << "Parallelepiped will not pass into the hole!" << endl;
+	}
+	system("pause");
+}
