6dda251e35fc02c48546d9dca9966a45ee8d4fc8
igortitov2004 <igor-titov-2004@mail.ru>
2021-09-29 20:03:13 +0300

    Задание 5

---
+++ Задание 5/Задание 5/Задание 5.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    int n1 = rand() % 10, n2 = rand() % 10, n3 = rand() % 10, n4 = rand() % 10;
+    for (int a = 0; a < 10; a++) {
+        for (int b =0; b < 10; b++) {
+            for (int c= 0; c <10; c++) {
+                for (int d = 0; d <10; d++) {
+                    if (a==n1 and b==n2 and  c == n3 and d==n4) cout<<"Finally the program guessed this GOOOOD password    "<<a<<b<<c<<d;
+                }
+
+            }
+        }
+    }
+    return 0;
+}
+
+// Надеюсь так норм,пойдет с int?
