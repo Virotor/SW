78631f9fc5d98b86edf8c7d81a845c839afe56d5
MariaBrichkovskaya <brichkovskayam@gmail.com>
2021-09-29 21:56:09 +0300

    feat: task 3

---
+++ задание 3/задание 3/задание 3.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int n, k, count = 0;                                           
+	cout << "Введите n и k" << endl;
+	cin >> n >> k;
+	k = to_string(n).length() - k;
+	n /= pow(10, k);
+	cout << n << endl;
+	system("pause");
+	return 0;
+}
