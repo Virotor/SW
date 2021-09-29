5202bf43b2995e928c0c26997f9b0ff8ee4eb188
mixasik03 <90718009+mixasik03@users.noreply.github.com>
2021-09-29 13:22:02 +0300

    Задание 3

---
+++ СР 1/СР 1/Задание 3.cpp
@@ -0,0 +1,11 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+int main() {
+    string n;
+    int k;
+
+    cout << "Enter n: "; cin >> n;
+    cout << "Enter k: "; cin >> k;
+    cout << "Number - " << n.substr(0, k);
+}
