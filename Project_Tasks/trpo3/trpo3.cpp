ccade3215a56ae22f827ef39426d4203226c6ef1
Koffdev <90766635+Koffdev@users.noreply.github.com>
2021-09-29 22:34:28 +0300

    init (task3) : add task 3

---
+++ Project_Tasks/trpo3/trpo3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int numK, numN;
+	cout << "Введите число n: ";
+	cin >> numN;
+	cout << "Введите число k: ";
+	cin >> numK;
+	int delitelNumn = 1;
+	while ((numN / delitelNumn) >= 1)
+	{
+		delitelNumn *= 10;
+	}
+	delitelNumn /= 10;
+	while (numK >= 1)
+	{
+		cout << (numN / delitelNumn) % 10;
+		delitelNumn /= 10;
+		numK--;
+	}
+	return 0;
+}
