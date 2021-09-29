f73d68b81c2b1dde47b6d94c72d5b541d76b4f88
andrejshemerej <90651422+andrejshemerej@users.noreply.github.com>
2021-09-29 18:06:15 +0300

    В прошлой версии были ошибки, а удалять я не умею

---
+++ 29.09.21/Task5.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    int y=0, n = 1;
+    int y1= (rand() % 10);
+    int y2 = (rand() % 10);
+    int y3 = (rand() % 10);
+    int y4 = (rand() % 10);
+    cout << y1 <<y2 <<y3<<y4 <<endl;
+    
+    for (int i=0; i <= y1; i++)
+    {  y++;}
+    cout << y-1;
+    y = 0;
+    for (int i = 0; i <= y2; i++)
+    {
+        y++;
+    }
+    cout << y - 1;
+    y = 0;
+    for (int i = 0; i <= y3; i++)
+    {
+        y++;
+    }
+    cout << y - 1;
+    y = 0;
+    for (int i = 0; i <= y4; i++)
+    {
+        y++;
+    }
+    cout << y - 1;
+
+
+    
+
+}
