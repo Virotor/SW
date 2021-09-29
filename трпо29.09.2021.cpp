0bb9fed8a485c3a0ccd6ead26a9cc76567514147
NastaKro <nasta.krogal1994@gmail.com>
2021-09-29 20:01:48 +0300

    SW-0

---
+++ трпо29.09.2021.cpp
@@ -0,0 +1,18 @@
+﻿// трпо29.09.2021.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "n";
+	cin >> n;
+	while (n)
+	{
+		cout << n % 10 << endl;
+		n /= 10;
+	}
+	return 0;
+}
