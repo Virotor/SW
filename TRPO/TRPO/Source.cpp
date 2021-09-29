8d1356e132ed913e577e2ede45af3f7c0fe22a70
gravity92041 <britvich.kostya@gmail.com>
2021-09-29 20:26:06 +0300

    feat: : zadanie2

--- TRPO/TRPO/Source.cpp
+++ TRPO/TRPO/Source.cpp
@@ -13,4 +13,36 @@ int main()
 		num /= 10;
 	}
 	system("pause");
+
+
+
+int day;
+	cout << "Введите номер дня недели\n"; cin >> day;
+	switch (day)
+	{
+	case(1):
+		cout << "Понедельник\n";
+		break;
+	case(2):
+		cout << "Вторник\n";
+		break;
+	case(3):
+		cout << "Среда\n";
+		break;
+	case(4):
+		cout << "Четверг\n";
+		break;
+	case(5):
+		cout << "Пятница\n";
+		break;
+	case(6):
+		cout << "Суббота\n";
+		break;
+	case(7):
+		cout << "Воскресенье\n";
+		break;
+	default:
+		cout << "Такого дня недели нет" << endl;
+	}
+	return 0;
 }
