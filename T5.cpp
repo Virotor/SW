248dd56a23fdb03b15175f559977352063b4203a
Linkler1864 <ivanovwrw@gmail.com>
2021-09-29 22:20:17 +0300

    init (T5):Task 5

---
+++ T5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int a[4], b[4], i, j;
+	srand(time(0));
+	for (i = 0;i < 4;i++) {
+		a[i] = rand() % 10;
+	}
+	for (i = 0;i < 4;i++)
+		for (j = 0;j < 10;j++)
+			if (a[i] == j) b[i] = j;
+	for (i = 0;i < 4;i++)
+		cout << b[i];
+}
