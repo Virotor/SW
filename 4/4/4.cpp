0fbb410b9b6b7b7ba0af4c532f3fd7e64f83ba67
Arseny <arsenkaman@gmail.com>
2021-09-29 20:50:51 +0300

    T4

---
+++ 4/4/4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    double Bricklong, BrickWidth, BrickHeigt;   
+    double Width, Height;          
+
+    cout << "Пройдёт кирпич или нет???" << endl;
+
+    cout << "Введите длинну параллелепипеда: ";
+    cin >> Bricklong;
+    cout << "Введите ширину параллелепипеда: ";
+    cin >> BrickWidth;
+    cout << "Введите высоту параллелепипеда: ";
+    cin >> BrickHeigt;
+
+    cout << "Введите ширина отверстия: ";
+    cin >> Width;
+    cout << "Введите высоту отверстия: ";
+    cin >> Height;
+
+    if (BrickWidth <= Width && BrickHeigt <= Height)
+    {
+        cout << "Параллелепипед пройдет в отверстие 1-й стороной" << endl;
+    }
+    else if (Bricklong <= Width && BrickHeigt <= Height)
+    {
+        cout << "Параллелепипед пройдет в отверстие 2-й стороной" << endl;
+    }
+    else if (Bricklong <= Width && BrickWidth <= Height)
+    {
+        cout << "Параллелепипед пройдет в отверстие 3-й стороной" << endl;
+    }
+    else
+    {
+        cout << "Параллелепипед не пройдет" << endl;
+    }
+
+    return 0;
+
+}
