6c45796ef86d07efe6afc41157280b91678dadfc
AlinaRaspberry <matalivic@mail.ru>
2021-09-29 22:58:55 +0300

    init(T1)

---
+++ zad1/zad1/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите целое число: ";
+	long long int celoeChislo;
+	cin >> celoeChislo;
+	celoeChislo = abs(celoeChislo);
+	cout << "Цифры данного числа: " << endl;
+	while (celoeChislo > 1 or celoeChislo < -1)
+	{
+		int ostatok;
+		ostatok = celoeChislo % 10;
+		celoeChislo = celoeChislo / 10;
+		cout << ostatok << endl;
+	}
+	system("pause");
+	return 0;
+}
