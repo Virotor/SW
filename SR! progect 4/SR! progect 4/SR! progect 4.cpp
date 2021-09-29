5dcb10c82684a2e26c1ece2362b75da664db96c9
CrypeDJ <litowrog@gmail.com>
2021-09-29 20:26:48 +0300

    init(T4):task 4

---
+++ SR! progect 4/SR! progect 4/SR! progect 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double a, b, c, r;
+    bool tf = false;
+    cout << "Введите размеры параллелепипеда ";
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c  = "; cin >> c;
+    cout << "Введите радиус отверстия ";
+    cout << "r = "; cin >> r;
+    if(pow(a * a + c * c, 0.5)<2*r) tf = true;
+    if (pow(a * a + c * c, 0.5) < 2*r) tf = true;
+    if (pow(c * c + b * b, 0.5) < 2*r) tf = true;
+    switch (tf) {
+    case true: {cout << "Пройдёт" << endl; } break;
+    case false: {cout << "Не пройдёт" << endl; } break;
+    }
+    return 0;
+}
