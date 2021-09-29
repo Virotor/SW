33c3773ead63daff239704aa2f89488e1532e398
CrypeDJ <litowrog@gmail.com>
2021-09-29 20:26:22 +0300

    init(T3):task 3

---
+++ SR1 progect 3/SR1 progect 3/SR1 progect 3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, k,col=0,n1;
+    cin >> n;
+    n1 = n;
+    while (n1 > 0) { n1 /= 10; col++; }
+    cin >> k;
+    if (k > col) cout << "Error number "; 
+    for (int i=0; i<col-k; i++) {
+        n /= 10;
+    }
+    cout << n;
+    return 0;
+}
