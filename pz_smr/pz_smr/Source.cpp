d4c44d2014f3ad8e808518316e43ef6ff97a331a
AnnSlizh <slizh20042004@gmail.com>
2021-09-29 22:23:36 +0300

    faet (Task) : add second task

--- pz_smr/pz_smr/Source.cpp
+++ pz_smr/pz_smr/Source.cpp
@@ -17,4 +17,35 @@ int main()
 	{
 		cout << str[i] << endl;
 	}
+
+	int numberOfDay;
+
+	cout << "Number of day = ";
+	cin >> numberOfDay;
+
+	switch (numberOfDay)
+	{
+	case 1: cout << "Monday" << endl;
+		break;
+
+	case 2: cout << "Thuestay" << endl;
+		break;
+
+	case 3: cout << "Wednesday" << endl;
+		break;
+
+	case 4: cout << "Thursday" << endl;
+		break;
+
+	case 5: cout << "Friday" << endl;
+		break;
+
+	case 6: cout << "Saturday" << endl;
+		break;
+
+	case 7: cout << "Sunday" << endl;
+		break;
+
+	default: cout << "Wrong number of day" << endl;
+	}
 }
