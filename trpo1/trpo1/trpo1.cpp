2c84fecfa0f82c8f615a3683c1cec5f33b8d3a3c
DedenV <dodopizzatrips@mail.ru>
2021-09-29 23:18:28 +0300

    init(Task#1):task1

---
+++ trpo1/trpo1/trpo1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int ch;
+	cout << "Введите число : ";
+	cin >> ch;
+	int del = 1;
+	while ((ch / del) >= 1)
+	{
+		del *= 10;
+	}
+	del = del / 10;
+	while (del >= 1)
+	{
+		cout << (ch / del) % 10 << endl;
+		del /= 10;
+	}
+}
