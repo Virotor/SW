31ef04eae75ad84dcb55b0cb4ddb4db7c2d929dd
LVerify <lisa.vetrova@gmail.com>
2021-09-29 17:42:54 +0300

    init(T3)

---
+++ lverifytask3/lverifytask3/lverifytask3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    long long int n, n1;
+    int k, i;
+    setlocale(LC_ALL, "RUS");
+    cout << "Введите два числа n, k, Первое число будет основным, второе будет означать какое количество цифер первого будет выведено";
+    cin >> n >> k;
+    n1 = n;
+    for (i = 0; n1 > 0; i++)
+    {
+        n1 /= 10;
+    }
+    i -= k;
+    for (int j=i;j>0;j--)
+    {
+        n /= 10;
+    }
+    cout << n << endl;
+    return 0;
+}
+
