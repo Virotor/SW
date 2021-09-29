6db55c113bfdbabd5d64d52388ce01b371d866ab
Simon-Zhuk <semenzhuk14@gmail.com>
2021-09-29 10:58:57 +0300

    SamWork2
    Task2

--- SamWork/SamWork/Source.cpp
+++ SamWork/SamWork/Source.cpp
@@ -6,11 +6,41 @@
 using namespace std;
 
 int main() {
-	string num;
+	/*string num;
 	cout << "Number= ";    cin >> num;
 	for (int i = 0; i < num.length(); i++) {
 		cout << num[i] << endl;
+	}*/ 
+
+	int day;
+	cout << "Day= ";    cin >> day;
+	switch (day) {
+	case 1:
+		cout << "Monday" << endl;
+		break;
+	case 2:
+		cout << "Tuesday" << endl;
+		break;
+	case 3:
+		cout << "Wednesday" << endl;
+		break;
+	case 4:
+		cout << "Thursday" << endl;
+		break;
+	case 5:
+		cout << "Friday" << endl;
+		break;
+	case 6:
+		cout << "Saturday" << endl;
+		break;
+	case 7:
+		cout << "Sunday" << endl;
+		break;
+	default:
+		cout << "Protrite ochki" << endl;
 	}
+
+
 	system("pause");
 	return 0;
 }
