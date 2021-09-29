3107f4d5d4b272a46ece9db4ec78b1cc4891a4c1
LVerify <lisa.vetrova@gmail.com>
2021-09-29 17:43:08 +0300

    init(T2)

---
+++ lverifytasks/lverifytasks/lverifytasks.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int week,chislo,ostatok;
+	setlocale(LC_ALL, "RUS");
+	//task2
+	cout << endl << "Ведите номер дня недели:";
+	cin >> week;
+	switch (week)
+	{
+	case 1:
+		cout << endl << "Понедельник"; break;
+	case 2:
+		cout << endl << "Вторник"; break;
+	case 3:
+		cout << endl << "Среда"; break;
+	case 4:
+		cout << endl << "Четверг"; break;
+	case 5:
+		cout << endl << "Пятница"; break;
+	case 6:
+		cout << endl << "Суббота"; break;
+	case 7:
+		cout << endl << "Воскресенье"; break;
+	default:
+		cout << endl << "такого дня недели не существует"; break;
+	}
+
+		return(0);
+	}
+
