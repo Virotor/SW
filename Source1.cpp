38ca11af994ef3d54ef3008ddd88f2deb46b75a4
vidu40k <90723662+vidu40k@users.noreply.github.com>
2021-09-29 22:16:47 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//#include<cmath>
+//#include<string>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	int num;
+//
+//	cout << "Введите номер дня недели" << endl;
+//	cin >> num;
+//
+//	switch (num)
+//	{
+//
+//	case 1: cout << "Понедельник" << endl;
+//		break;
+//	case 2: cout << "Вторник" << endl;
+//		break;
+//	case 3: cout << "Среда" << endl;
+//		break;
+//	case 4: cout << "Четверг" << endl;
+//		break;
+//	case 5: cout << "Пятница" << endl;
+//		break;
+//	case 6: cout << "Суббота" << endl;
+//		break;
+//	case 7: cout << "Воскресенье" << endl;
+//		break;
+//	default:
+//		cout << "Такого дня нет" << endl;
+//		break;
+//	}
+//}
