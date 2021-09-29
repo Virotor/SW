464caab4c2cf0ef304471e89e53ba974a09779d3
AndreiZastupnevich <andreizastupnevich@gmail.com>
2021-09-29 22:20:30 +0300

    3
    type (scope) : short summary
    
    type : init
    
    scope : T1
    
    short summary :  solution of the task

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, k, n1, razm = 0;
+	cout << "Введите числа n и k" << endl;
+	cin >> n >> k;
+	n1 = n;
+	while (n1 > 0)
+	{
+		razm++;
+		n1 /= 10;
+	}
+	for (int i = 0; i < (razm - k); i++)
+	{
+		n /= 10;
+	}
+	cout << "Число, содержащее первые k цифр числа n: " << n;
+	return 0;
+}
