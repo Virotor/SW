8561589674c98877dba04f9abf31dce262f08583
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-09-29 22:11:45 +0300

    Task 1

---
+++ TrpoSW1/TrpoSW1/TrpoSW1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void splittingNumbers() {
+	int number;
+	cout << "enter the number, each digit of this number must be printed on a new line: ";
+	cin >> number;
+	while (number) {
+		cout << number % 10 << endl;
+		number /= 10;
+	}
+}
+
+int main() {
+	splittingNumbers();
+	return 0;
+}
