710f993e6f7f43a10611d4b12c52befae14dd0ba
Vadim Tereshko <krolikrodger18895@gmail.com>
2021-09-30 00:00:51 +0300

    init(Task#1):Zadanie1

---
+++ Zadanie1/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string s;
+	cout << "Введите номер = ";
+	cin >> s;
+	for (int i = 0; i < size(s); i++)
+		cout << s[i] << endl;
+	return 0;
+}
