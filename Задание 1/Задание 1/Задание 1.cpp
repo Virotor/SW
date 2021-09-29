897f36fd56d51b59d08ffcc1f78022079f46be59
georgezxc <george.github@bk.ru>
2021-09-30 00:00:08 +0300

    Задание 1

---
+++ Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int c, z, x = 0;
+	cout << "enter a number" << endl;
+	cin >> c;
+	while (c > 0)
+	{
+		z = c % 10;
+		x = x * 10 + z;
+		c = c / 10;
+	}
+	while (x > 0)
+	{
+		cout << x % 10 << " " << endl;
+		x = x / 10;
+	}
+}
