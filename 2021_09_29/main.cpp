dd06ea36382e068364c3c27401a632c0cb902b85
Viktor <375291163332@mail.ru>
2021-09-29 11:14:42 +0300

    Task2

--- 2021_09_29/main.cpp
+++ 2021_09_29/main.cpp
@@ -37,11 +37,40 @@ void task1() {
         value /= 10;
     }
     cout << "Task1 is completed\n";
-    while(value)
 }
 
 void task2() {
-
+    cout << "Task2:\n";
+    cout << "Enter the number of the day of the week (1-7)\n";
+    cout << "Day = ";
+    int day_number;
+    cin >> day_number;
+    switch(day_number) {
+        case 1:
+            cout << "Monday\n";
+            break;
+        case 2:
+            cout << "Tuesday\n";
+            break;
+        case 3:
+            cout << "Wednesday\n";
+            break;
+        case 4:
+            cout << "Thursday\n";
+            break;
+        case 5:
+            cout << "Friday\n";
+            break;
+        case 6:
+            cout << "Saturday\n";
+            break;
+        case 7:
+            cout <<  "Sunday\n";
+            break;
+        default:
+            cout << "Input error\n";
+    }
+    cout << "Task2 is completed\n";
 }
 
 void task3() {
