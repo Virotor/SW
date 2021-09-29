4b98720c1576c85a9ced01774756cf31dc4cb591
Batory05 <maximbaturo@gmail.com>
2021-09-29 23:32:15 +0300

    feat: task 1

---
+++ SWmy.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <iostream>
+
+int main()
+{
+
+    std::string number;
+
+    std::cout << "Enter the number:" << '\n';
+    std::cin >> number;
+    
+    for (int i = 0; i < size(number); i++) {
+        std::cout << number[i] << '\n';
+    }
+
+    system("pause");
+}
