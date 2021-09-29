98fd003dbcc2d57fa4509d5140a75cf8dfce067b
georgezxc <george.github@bk.ru>
2021-09-30 00:01:05 +0300

    Задание 5

---
+++ Задание 5/Задание 5/Задание 5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+using namespace std;
+
+
+int GetRandomNumber(int min, int max)
+{	
+	srand(time(NULL));
+		int num = min + rand() % (max - min + 1);
+		int num2 = min + rand() % (max - min + 1);
+		int num3 = min + rand() % (max - min + 1);
+		int num4 = min + rand() % (max - min + 1);
+
+		return num; return num2; return num3; return num4;
+	}
+
+
+void main()
+{
+	
+	int number;
+	number = GetRandomNumber(0, 6); 
+	cout << " " << number;;
+
+	
+	int number2;
+	number2 = GetRandomNumber(0, 3); 
+	cout << " " << number2;;
+
+	
+	int number3;
+	number3 = GetRandomNumber(0, 9); 
+	cout << " " << number3;;
+
+
+	int number4;
+	number4 = GetRandomNumber(0, 5); 
+	cout << " " << number4;;
+}
