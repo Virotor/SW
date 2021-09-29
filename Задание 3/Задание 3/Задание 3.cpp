b439e4295701952cb4443a9cde041061ed0400ab
igortitov2004 <igor-titov-2004@mail.ru>
2021-09-29 16:35:16 +0300

    Задание 3

---
+++ Задание 3/Задание 3/Задание 3.cpp
@@ -0,0 +1,11 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+int main() {
+    string n;
+    int k;
+
+    cout << "input n "; cin >> n;
+    cout << "input k: "; cin >> k;
+    cout << "Number is " << n.substr(0, k);
+}
