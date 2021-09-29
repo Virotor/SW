97b5dd4fbca0e6867f29db3fbad4cdfe4daf95d6
KozelIgor <kozeligor99@gmail.com>
2021-09-29 18:05:42 +0300

    Task 1
    -создание проекта
    -написано первое задание

---
+++ самостоятельная_ работа/самостоятельная_ работа/самостоятельная_ работа.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include<locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	cout << "Введите номер задания: ";
+	cin >> n;
+	switch (n)
+	{
+	case 1:
+		void first();
+		{
+			int num;
+			cout << "Задание 1: Выведение цифр числе в отдельных строках." << endl << endl;
+			cout << "Введите число: ";
+			cin >> num;
+			while (num)
+			{
+				cout << num % 10 << endl;
+				num /= 10;
+			}
+		}
+		break;
