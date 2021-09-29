8fed46bd149624606b443fe7a29da788817d5700
igortitov2004 <igor-titov-2004@mail.ru>
2021-09-29 16:34:41 +0300

    Задание 1

---
+++ Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string n;
+    cout << "input number " << endl;
+    cin >> n;
+    for (int i = 0; i < size(n); i++)
+    cout << n[i] << endl;
+
+
+    return 0;
+}
+
