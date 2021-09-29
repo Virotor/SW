3de00de2ba3c68701e9fa77b10a5a55c384995c5
Pishchako Maksim <90351778+Deriere@users.noreply.github.com>
2021-09-29 18:26:32 +0300

    SDT_SW_Task2

--- SDT_SW1/SDT_SW1/Source.cpp
+++ SDT_SW1/SDT_SW1/Source.cpp
@@ -11,6 +11,36 @@ int main() {
 	}
 
 
+	int NumTask2;
+	cout << "Enter the day of the week"; cin >> NumTask2;
+	switch (NumTask2) {
+	case 1:
+		cout << "Monday" << endl;
+		break;
+	case 2:
+		cout << "Tuesday" << endl;
+		break;
+	case 3:
+		cout << "Wednesday" << endl;
+		break;
+	case 4:
+		cout << "Thursday" << endl;
+		break;
+	case 5:
+		cout << "Friday" << endl;
+		break;
+	case 6:
+		cout << "Saturday" << endl;
+		break;
+	case 7:
+		cout << "Sunday" << endl;
+		break;
+
+	default:
+		cout << "There is no such day of the week" << endl;
+	}
+
+
 	
 
 	return 0;
