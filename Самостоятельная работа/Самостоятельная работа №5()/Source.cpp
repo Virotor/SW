3ff78b4df6906168d1ead347e00c660aa423c87b
lesik2 <lesikpuhalskij@gmail.com>
2021-09-29 19:47:10 +0300

    Задание №2

---
+++ Самостоятельная работа/Самостоятельная работа №5()/Source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int n;
+    cout << "Введите номер недели" << endl;
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+    {
+        cout << "Понедельник" << endl;
+        break;
+    }
+    case 2:
+    {
+        cout << "Вторник" << endl;
+        break;
+    }
+    case 3:
+    {
+        cout << "Среда" << endl;
+        break;
+    }case 4:
+    {
+        cout << "Четверг" << endl;
+        break;
+    }case 5:
+    {
+        cout << "Пятница" << endl;
+        break;
+    }case 6:
+    {
+        cout << "Суббота" << endl;
+        break;
+    }
+    case 7:
+    {
+        cout << "Воскресенье" << endl;
+        break;
+    }
+    default:
+    {
+        cout << "Введен неправильный номер недели" << endl;
+        break;
+    }
+    }
+
+}
