6c288a7b2953b56554639fb5750cfe3bc6a0835d
mixasik03 <o.mixasik2003@yandex.ru>
2021-09-29 23:14:19 +0300

    Задание 1

---
+++ самостоятельная 1/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string p;
+	cout << "Enter number- "; cin >> p;
+	for (int i = 0; i < size(p); i++) cout << p[i] << endl;
+	return 0;
+}
