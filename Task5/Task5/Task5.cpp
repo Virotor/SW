472c4c8cf28d785d4a3d9578b6e846b6863f0ce0
rekinx <makary787898@gmail.com>
2021-09-29 22:55:09 +0300

    init(T5) : Task 5

---
+++ Task5/Task5/Task5.cpp
@@ -0,0 +1,42 @@
+﻿#include<string>
+#include<iostream>
+#include <random>
+using namespace std;
+int main() {
+int num1 = rand() % 10, num2 = rand() % 10, num3 = rand() % 10, num4 = rand() % 10, num;         //5 задание
+string hid_num, ans = "0000";
+
+hid_num = to_string(num1) + to_string(num2) + to_string(num3) + to_string(num4);
+
+for (int i = 0; i <= 9; i++) {
+	ans.replace(3, 1, to_string(i));
+	if (ans == hid_num) {
+		cout << "password: " << ans << endl;
+	}
+	for (int j = 0; j <= 9; j++) {
+		ans.replace(2, 1, to_string(j));
+		if (ans == hid_num) {
+			cout << "password: " << ans << endl;
+		}
+		for (int l = 0; l <= 9; l++) {
+			ans.replace(1, 1, to_string(l));
+			if (ans == hid_num) {
+				cout << "password: " << ans << endl;
+			}
+			for (int f = 0; f <= 9; f++) {
+				ans.replace(0, 1, to_string(f));
+				if (ans == hid_num) {
+					cout << "password: " << ans << endl;
+				}
+			}
+		}
+	}
+}
+
+system("pause");
+
+
+return 0;
+
+
+}
