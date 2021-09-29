23f14a42d9c63a229f8c4f577466bb130640bd90
KirillPuzevich <puzevichk@mail.ru>
2021-09-29 21:44:32 +0300

    feat(Task): add task 1

---
+++ Sam rab/Sam rab/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n)
+    {
+
+        cout << n % 10 << endl;
+        n /= 10;
+
+
+
+    }
+    system("pause");
+    return 0;
+}
