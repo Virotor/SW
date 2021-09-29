cd44d4463bcd041d4964df996bb3b9427db9501e
Esed-off <90351236+Esed-off@users.noreply.github.com>
2021-09-29 12:32:40 +0300

    первое задание

--- Sw 1.cpp
+++ Sw 1.cpp
@@ -1,15 +1,19 @@
 #include "pch.h"
 #include <iostream>
-#include <math.h>
+#include "math.h"
+
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "RUS");
+    int n, i;
+    cout << "введите число "; cin >> n;
+    while (n != 0) 
     {
-        setlocale(LC_ALL, "RUS");
-        cout << "RAND_MAX = " << RAND_MAX << endl; // константа, хранящая максимальный предел из интервала случайных чисел
-        cout << "random number = " << rand() << endl; // запуск генератора случайных чисел
-        system("pause");
-        return 0;
+        i = n % 10;
+        n = n / 10;
+        cout << i << endl;
     }
+    return 0;
 }
