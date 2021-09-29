4efc0a97c78b8256de92fe5a1f6e37be8e97f7f1
Ingarondel <inga404@icloud.com>
2021-09-29 23:05:03 +0300

    Feat: made task 2
    Составить программу, которая по введенному номеру дня недели будет выводить его название. Нумерация дней недели начинается с 1 – понедельник, 2 — вторник и т. д. 1 балл

---
+++ Самостоятельная работа 1/Задание 2/Задание 2/Задание 2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	//2 задание
+	setlocale(LC_ALL, "rus");
+	int day;
+	cout << "Введите порядковый номер дня недели" << endl;
+	cin >> day;
+	switch (day)
+	{
+	case 1:
+		cout << "Понедельник" << endl;
+		break;
+	case 2:
+		cout << "Вторник" << endl;
+		break;
+	case 3:
+		cout << "Среда" << endl;
+		break;
+	case 4:
+		cout << "Четверг" << endl;
+		break;
+	case 5:
+		cout << "Пятница" << endl;
+		break;
+	case 6:
+		cout << "Суббота" << endl;
+		break;
+	case 7:
+		cout << "Воскресенье" << endl;
+		break;
+	default: cout << "такого дня недели нет" << endl;
+		break;
+	}
+	system("pause");
+	return 0;
+}
