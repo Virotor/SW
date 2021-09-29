4dfaa0ddee49e36fe89fed0740e57a5da795f546
Lightning-Andrey <andrejkamors@gmail.com>
2021-09-29 15:09:45 +0300

    feat(Task#1):feat'n'fix
    Расширил возможности первой программы и добавил вторую

--- ТРПО СР №1/Source.cpp
+++ ТРПО СР №1/Source.cpp
@@ -6,8 +6,7 @@ using namespace std;
 int main()
 {	
 	cout << "\n-----------PROGRAM#1-----------------------------------------------------------------\n";
-	int factor = 1;
-	double number;
+	long double number, factor = 1 ;
 	do //proverka
 	{
 		cout << "\n\tEnter positive, preferably int number: ";
@@ -16,15 +15,29 @@ int main()
 	} while ( number<= 0  or trunc(number) < number);
 
 	while (factor < number/10) factor *= 10;
+
 	while (factor >= 1)
 	{
-		cout << "\t" << (int)number / factor << endl;
-		number -= (double)((int)number / factor)*factor;
+		cout<< "\t" << (int)(number / factor) << endl;
+		number -= (int)(number / factor)*factor;
 		factor /= 10;
 	}
 
+	cout << "\n-----------PROGRAM#2-----------------------------------------------------------------\n";
 
-
-
+	int day;
+	cout << "\n\tEnter the number of weekday: ";
+	cin >> day;
+	switch (day)
+	{
+	case 1:cout << "\tMonday" << endl; break;
+	case 2:cout << "\tTuesday" << endl; break;
+	case 3:cout << "\tWednesday" << endl; break;
+	case 4:cout << "\tThursday" << endl; break;
+	case 5:cout << "\tFriday" << endl; break;
+	case 6:cout << "\tSaturday" << endl; break;
+	case 7:cout << "\tSunday" << endl; break;
+	default: cout << "Incorrect number...";
+	}
 
 }
