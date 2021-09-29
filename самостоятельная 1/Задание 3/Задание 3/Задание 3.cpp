9d370ec7b2292751b7f14e8ea2c144cc10bf02c1
mixasik03 <o.mixasik2003@yandex.ru>
2021-09-29 23:15:04 +0300

    Задание 3

---
+++ самостоятельная 1/Задание 3/Задание 3/Задание 3.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string n;
+    cout << "n=  "; cin >> n;
+    int k;
+    cout << "k=  "; cin >> k;
+    if (k <= size(n))
+    {
+        for (int i = 0; i < k; i++) {
+            cout << n[i];
+        }
+    }
+    else cout << "NOPE";
+    return 0;
+}
