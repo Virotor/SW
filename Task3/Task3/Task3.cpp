45567996dab3ee820561c00b7a573cc752248503
rekinx <makary787898@gmail.com>
2021-09-29 22:19:49 +0300

    init (T3) : Task 3

---
+++ Task3/Task3/Task3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int n, k;
+	cout << "n = ";
+	cin >> n;
+	cout << "k = ";
+	cin >> k;
+	k = to_string(n).length() - k;
+	n /= pow(10, k);
+	cout << n << endl;
+	return 0;
+}
