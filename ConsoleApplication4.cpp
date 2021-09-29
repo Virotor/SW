59acea9d00c3c8c22000b6b51d36e26135e37ffa
Daniil Marchenko <Daniiik1509@yandex.ru>
2021-09-29 21:19:13 +0300

    commit4
    Написать программу, которая должна определить, пройдёт ли параллелепипед в отверстие. Размеры отверстия и габариты параллелепипеда (длина, высота, ширина) вводятся пользователем.

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+	double a, b, c, d, e;
+	cout << "Длина отверстия ";
+	cin >> a;
+	if (a <= 0)return 0;
+	cout << "Ширина отверстия ";
+	cin >> b;
+	if (b <= 0)return 0;
+	cout << "Длина параллелепипеда ";
+	cin >> c;
+	if (c <= 0)return 0;
+	cout << "Ширина параллелепипеда ";
+	cin >> d;
+	if (d <= 0)return 0;
+	cout << "Высота параллелепипеда ";
+	cin >> e;
+	if (e <= 0)return 0;
+	if (a < c || b < e )
+		cout << "Параллелепипед не проходит в отверстие";
+	else cout << "Параллелепипед проходит в отверстие";
+
+	return 0;
+}
+
