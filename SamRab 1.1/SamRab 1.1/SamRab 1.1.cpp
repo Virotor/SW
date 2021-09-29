f13113c4bcd68af3e45452fcb02ec24c0126626f
DmitryPasmurtsev <dima427614@gmail.com>
2021-09-29 20:05:07 +0300

    SamRab 1.1
    init commit

---
+++ SamRab 1.1/SamRab 1.1/SamRab 1.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+	cout << "number = "; cin >> s;
+	for (int i = 0; i < size(s); i++) cout << s[i] << endl;
+	return 0;
+}
