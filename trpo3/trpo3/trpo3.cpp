dcd3f772aefac20d16046d108a3d11c9d4667978
DedenV <dodopizzatrips@mail.ru>
2021-09-29 23:19:33 +0300

    init(Task#3):task3

---
+++ trpo3/trpo3/trpo3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int chislo, kolichestvoCifr;
+	cout << "Введите число : "; cin >> chislo;
+	cout << "Введите нужное кол-во цифр для вывода : "; cin >> kolichestvoCifr;
+	int kol=1 , povtn;
+	povtn = chislo;
+	while ((chislo / kol) >= 1)
+	{
+		kol *= 10;
+	}
+	kol /= 10;
+	while (kolichestvoCifr >= 1)
+	{
+		cout << (chislo / kol) % 10;
+		kol /= 10;
+		kolichestvoCifr--;
+	}
+}
