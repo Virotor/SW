2c38bbe8cfc1a5cd549ac71a82dbfe894a6d7a6d
Viktor <375291163332@mail.ru>
2021-09-29 11:01:36 +0300

    Task1

---
+++ 2021_09_29/main.cpp
@@ -0,0 +1,57 @@
+/**
+      ---------------------------------------------------
+      |         Made by Shamal Viktor (@j3uktop)        |
+      |           2019-2021 - Lyceum BSU - IM           |
+      |             2021 - BSUIR - FCP - PMS            |
+      |          *** All rights reserved ***            |
+      ---------------------------------------------------
+                                            			**/
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+void task1();
+void task2();
+void task3();
+void task4();
+void task5();
+
+int main() {
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+    return 0;
+}
+
+void task1() {
+    long long value;
+    cout << "Task1:\n";
+    cout << "Value = ";
+    cin >> value;
+    cout << "Digits of value:\n";
+    while(value) {
+        cout << value % 10 << '\n';
+        value /= 10;
+    }
+    cout << "Task1 is completed\n";
+    while(value)
+}
+
+void task2() {
+
+}
+
+void task3() {
+
+}
+
+void task4() {
+
+}
+
+void task5() {
+
+}
