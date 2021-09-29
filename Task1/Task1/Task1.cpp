da4b18bbf09204579cac623c05df4c35d7c3eea4
rekinx <makary787898@gmail.com>
2021-09-29 22:10:49 +0300

    init(T1) : Task1

---
+++ Task1/Task1/Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout << "Enter the number:  "; cin >> num;
+	int count = 1;
+	for (int i = 0; i < count; i++)
+	{
+		cout << num % 10 << endl; num /= 10;
+		if (num != 0)
+		{
+			count++;
+		}
+	}
+	return 0;
+
+
+
+}
