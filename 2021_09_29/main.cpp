a17dc36f0896029cbcf63b0cc4899e28312fe543
Viktor <375291163332@mail.ru>
2021-09-29 11:30:11 +0300

    Task3

--- 2021_09_29/main.cpp
+++ 2021_09_29/main.cpp
@@ -7,14 +7,19 @@
       ---------------------------------------------------
                                             			**/
 #include <iostream>
+#include <conio.h>
 
 using std::cout;
 using std::cin;
 
 void task1();
+
 void task2();
+
 void task3();
+
 void task4();
+
 void task5();
 
 int main() {
@@ -32,7 +37,7 @@ void task1() {
     cout << "Value = ";
     cin >> value;
     cout << "Digits of value:\n";
-    while(value) {
+    while (value) {
         cout << value % 10 << '\n';
         value /= 10;
     }
@@ -45,7 +50,7 @@ void task2() {
     cout << "Day = ";
     int day_number;
     cin >> day_number;
-    switch(day_number) {
+    switch (day_number) {
         case 1:
             cout << "Monday\n";
             break;
@@ -65,7 +70,7 @@ void task2() {
             cout << "Saturday\n";
             break;
         case 7:
-            cout <<  "Sunday\n";
+            cout << "Sunday\n";
             break;
         default:
             cout << "Input error\n";
@@ -74,7 +79,38 @@ void task2() {
 }
 
 void task3() {
-
+    long long value;
+    long long out_digits;
+    long long count_of_digits = 0;
+    long long out_value = 0;
+    long long mod = 1;
+    cout << "Task3:\n";
+    cout << "Value = ";
+    cin >> value;
+    cout << "K = ";
+    cin >> out_digits;
+    while(value) {
+        out_value = out_value * 10 + value % 10;
+        value /= 10;
+        count_of_digits++;
+    }
+    for(int i = out_digits; i < count_of_digits; i++) {
+        mod *= 10;
+    }
+    if(count_of_digits < out_digits) {
+        cout << "Error. Count of digits in value is too small\n";
+        cout << "Try again? Y/N\n";
+        char user_answ = getche();
+        if(user_answ == 'Y' || user_answ == 'y') {
+            task3();
+        } else {
+            cout << "Task3 is completed\n";
+        }
+    } else {
+        out_value /= mod;
+        cout << out_value << '\n';
+        cout << "Task3 is completed\n";
+    }
 }
 
 void task4() {
