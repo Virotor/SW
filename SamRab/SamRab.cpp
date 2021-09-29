7dfce3885f47a39f89e0ca15f4d271ce6df0b7df
AlexeyStepanchuk <alekseystepanchuk@mail.ru>
2021-09-29 21:09:07 +0300

    1stTask

---
+++ SamRab/SamRab.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void number(int& number) {
+    cout << "number = ";
+    cin >> number;
+    while (number) {
+        cout << number % 10 << endl;
+        number /= 10;
+    }
+}
+int main() {
+    int x;
+    number(x);
+    system("pause");
+    return 0;
+}
