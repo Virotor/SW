5dd3c4e8efd5e866b553d5ba1d89efe634b5c586
Varvashenyaa <alena.varvashenya@gmail.com>
2021-09-29 22:17:51 +0300

    add task 3

---
+++ задание 3 версия не знаю какая/задание 3 версия не знаю какая/задание 3 версия не знаю какая.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a, k;
+	cout << "Введите число : "; cin >> a;
+	cout << "Введите нужное кол-во цифр для вывода : "; cin >> k;
+	int kol = 1, p;
+	p = a;
+	while ((a / kol) >= 1)
+	{
+		kol *= 10;
+	}
+	kol /= 10;
+	while (k >= 1)
+	{
+		cout << (a / kol) % 10;
+		kol /= 10;
+		k--;
+	}
+}
