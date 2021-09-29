c371033644ea895c7b681c1447a795f5665ea465
Batory05 <maximbaturo@gmail.com>
2021-09-29 23:48:55 +0300

    feat: task 2

--- SWmy.cpp
+++ SWmy.cpp
@@ -13,5 +13,49 @@ int main()
         std::cout << number[i] << '\n';
     }
 
-    system("pause");
+
+    // task 2
+
+int number;
+
+std::cout << "Enter the number of day in week:" << '\n';
+std::cin >> number;
+
+switch (number) {
+
+    case 1:
+      std::cout << "Понедельник." << '\n';
+      break;
+
+    case 2:
+      std::cout << "Вторник." << '\n';
+      break;
+
+    case 3:
+      std::cout << "Среда." << '\n';
+      break;
+
+    case 4:
+      std::cout << "Четверг." << '\n';
+      break;
+
+    case 5:
+      std::cout << "Пятница." << '\n';
+      break;
+
+    case 6:
+      std::cout << "Суббота." << '\n';
+      break;
+
+    case 7:
+      std::cout << "Воскресенье." << '\n';
+      break;
+
+    default:
+      std::cout << "ERROR" << '\n';
+
+}
+
+system("pause");
+
 }
