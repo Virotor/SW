7e1b6917ac7643ac15fd1ba203c43e37e4f72f21
DiMoNiO04 <91553443+DiMoNiO04@users.noreply.github.com>
2021-09-29 22:09:26 +0300

    commit
    type: init
    scope: T1
    short summary: task1 completed

--- SM_1/SM_1/Source.cpp
+++ SM_1/SM_1/Source.cpp
@@ -32,3 +32,24 @@ void task1() {
 	}
 	cout << endl;
 }
+
+
+void task2() {
+	cout << "Exercises 2\n";
+	int WEEK_NUM;
+	cout << "Enter the day number "; cin >> WEEK_NUM;
+	if (WEEK_NUM > 7)
+		cout << "Error";
+	else {
+		switch (WEEK_NUM) {
+		case 1: cout << "Monday" << endl; break;
+		case 2: cout << "Tuesday" << endl; break;
+		case 3: cout << "Wednesday" << endl; break;
+		case 4: cout << "Thursday" << endl; break;
+		case 5: cout << "Friday" << endl; break;
+		case 6: cout << "Saturday" << endl; break;
+		case 7: cout << "Sunday" << endl; break;
+		}
+	}
+	cout << endl << endl;
+}
