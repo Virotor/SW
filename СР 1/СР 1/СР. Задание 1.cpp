eef6001a9b172f550eb532a97936f674f16db67e
mixasik03 <o.mixasik2003@yandex.ru>
2021-09-29 12:39:02 +0300

    Задание 1

---
+++ СР 1/СР 1/СР. Задание 1.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "a= ";
+    cin >> a;
+    while (a) {
+        a = a / 10; 
+        cout << a % 10 << endl;   
+    }
+    return 0;
+}
