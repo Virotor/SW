6cdaa61f5eba230151036e01307d10696b753f10
Daniil Marchenko <Daniiik1509@yandex.ru>
2021-09-29 21:18:36 +0300

    commit1
    Составить программу, которая будет считывать введённое целое число. После чего, каждую цифру этого числа необходимо вывести в новой строке.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+	string a;
+	cout << "Введите число " << endl;
+	cin >> a;
+	for (int b = 0; b < size(a); b++)
+		cout << a[b] << endl;
+	return 0;
+}
+
