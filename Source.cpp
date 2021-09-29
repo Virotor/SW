ac6d6a8d278d5798538050e5a4e23f610a7ed8b5
vidu40k <90723662+vidu40k@users.noreply.github.com>
2021-09-29 22:16:22 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include<cmath>
+//#include<string>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	int line;
+//	cout << "Введите число" << endl;
+//	const int SIZE = 255;
+//	char arr[SIZE];
+//	gets_s(arr);
+//	int _long = strlen(arr);
+//	for (int i = 0; i < _long; i++)
+//	{
+//		cout << arr[i] << endl;
+//	}
+//
+//}
