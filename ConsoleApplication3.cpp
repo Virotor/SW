1289470445c9c190cd817fe00bcbac08882ae80e
Daniil Marchenko <Daniiik1509@yandex.ru>
2021-09-29 21:19:01 +0300

    commit3
    Написать программу, которая будет принимать два целых числа n и k и возвращает число, содержащее k первых цифр числа n.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string a;
+	int b;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	if (b <= size(a))
+	{
+		for (int c = 0; c < b; c++) {
+			cout << a[c] << endl;
+		}
+	}
+	else cout << "ERROR" << endl;
+	return 0;
+}
+
