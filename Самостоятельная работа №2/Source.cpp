500f6abdf798e036f3024e4cb0bd049a337e64d1
lesik2 <lesikpuhalskij@gmail.com>
2021-09-29 20:08:59 +0300

    Задание №1

---
+++ Самостоятельная работа №2/Source.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int n, k;
+	const int SIZE = 100;
+	int arr[SIZE];
+	int d, i = 0;
+	cout << "¬ведите число,число должно быть положительным";
+
+
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << "„исло должно быть положительным" << endl;
+		return;
+	}
+	cout << "¬ведите второе число " << endl;
+	cin >> k;
+	
+	
+	
+	
+	for (; n > 0;)
+	{
+		d = n % 10;
+		arr[i] = d;
+		n = n / 10;
+		i++;
+	}
+	i--;
+	for (int c = i; c > i-k; c--)
+	{
+		cout << arr[c];
+	}
+
+
+}
