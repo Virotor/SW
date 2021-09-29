ea9149806af2fe835adeeb87b3178e7bd09cdfe3
Shveder <89966688+Shveder@users.noreply.github.com>
2021-09-29 15:59:07 +0300

    sw задание 1
    Вывел каждое цифру числа в новой строке.Но,нужно ли чтобы они были по порядку?

---
+++ sw ex 1/sw ex 1/Source.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	printf("Vvedite chislo:\n");
+	int vvedenoeChislo;
+	scanf_s("%d", &vvedenoeChislo);
+	int ostatokOtDeleniya;
+	ostatokOtDeleniya = 0;
+	for (int i = 0; i <=vvedenoeChislo; i++)
+	{
+		ostatokOtDeleniya = vvedenoeChislo % 10;
+		vvedenoeChislo = vvedenoeChislo / 10;
+
+		printf("%d\n", ostatokOtDeleniya);//Так сойдёт ? Или нужно по порядку?
+	}
+	return 0;
+}
