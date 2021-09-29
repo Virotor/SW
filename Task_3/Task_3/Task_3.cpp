4d7ccfb3612f3f54694cfbc468ba636be7869c8f
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2021-09-29 17:04:12 +0300

    init T3

---
+++ Task_3/Task_3/Task_3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    long long int n, clone;
+    int k, i;
+
+    cout << "Введите n и k" << endl;
+    cin >> n >> k;
+
+    clone = n;
+
+    for (i = 0; clone > 0; i++)
+    {
+        clone /= 10;
+    }
+
+    i -= k;
+
+    for (int j = i; j > 0; j--)
+    {
+        n /= 10;
+    }
+
+    cout << n << endl;
+
+    return 0;
+}
