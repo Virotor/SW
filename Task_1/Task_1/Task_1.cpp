809d51ea8896c2dce4049bff4a74065c47f305a6
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2021-09-29 17:02:06 +0300

    init T1:

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    long long int number;
+
+    cout << "Введите целое число" << endl;
+    cin >> number;
+    number = abs(number);
+
+    while (number)
+    {
+        cout << number % 10 << endl;
+        number /= 10;
+    }
+}
