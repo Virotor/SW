73e82804a143c6864b99f6611c1f6f53fbd7e216
LVerify <lisa.vetrova@gmail.com>
2021-09-29 17:42:36 +0300

    init(T1)

---
+++ lverifytask1/lverifytask1/lverifytask1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	long long int chislo;
+	int ostatok;
+	cout << endl << "введите пожалуйста число" << endl;
+	cin >> chislo;
+	chislo = abs(chislo);
+	while (chislo >= 1)
+	{
+		ostatok = chislo % 10;
+		cout << ostatok << endl;
+		chislo = chislo / 10;
+	}
+	return(0);
+}
+
