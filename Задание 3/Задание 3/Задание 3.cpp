e57b988d25f7b690b88c08dee3ad793f86a225fa
Anycart <sheyngis03@yandex.ru>
2021-09-29 18:57:15 +0300

    Задание 3

---
+++ Задание 3/Задание 3/Задание 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int k;
+	string n;
+	cout << "Введите n" << endl;
+	cin >> n;
+	cout << "Введите k" << endl;
+	cin >> k;
+	if (k <= size(n))
+	{
+		for (int i = 0; i < k;i++)
+		{
+			cout << n[i];
+		}
+	}
+	else cout << "Не верно";
+	return 0;
+}
