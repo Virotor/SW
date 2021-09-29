e65f374cbb711d696ff5c7cb23d382a8d1b6538e
AndreiZastupnevich <andreizastupnevich@gmail.com>
2021-09-29 22:21:19 +0300

    4
    type (scope) : short summary
    
    type : init
    
    scope : T4
    
    short summary :  solution of the task

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "Russian");
+	int Width, Height, width, height, length;
+	cout << "Введите значения ширины и высоты отверстия; ширины, высоты и длины параллелограмма" << endl;
+	cin >> Width >> Height >> width >> height >> length;
+	if ((Width >= width && Height >= height) ||
+		(Width >= width && Height >= length) ||
+		(Width >= height && Height >= width) ||
+		(Width >= height && Height >= length) ||
+		(Width >= length && Height >= width) ||
+		(Width >= length && Height >= height)) cout << "Пройдёт";
+	else cout << "Не пройдёт";
+	return 0;
+}
