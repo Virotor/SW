549bff826dda2dd6ab1e2510d63b4b331b0d054a
Ingarondel <inga404@icloud.com>
2021-09-29 23:03:19 +0300

    Feat: made task 1
    Составить программу, которая будет считывать введённое целое число. После чего, каждую цифру этого числа необходимо вывести в новой строке. 1 балл

---
+++ Самостоятельная работа 1/Задание 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,21 @@
+﻿#include <math.h>
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	//1 задание
+	setlocale(LC_ALL, "rus");
+
+	int number;
+	cout << "Введите целое число" << endl << endl;
+	cin >> number;
+	number = abs(number);
+	do {
+		cout << number % 10 << endl;
+		number = number / 10;
+	} while (number > 0);
+	system("pause");
+	return 0;
+}
