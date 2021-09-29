edfcf131f363c52982fed2a5b3d80f497e89a739
mixasik03 <90718009+mixasik03@users.noreply.github.com>
2021-09-29 14:40:50 +0300

    Задание 4

---
+++ СР 1/СР 1/Задание 4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double vis, sh, t, vis1, sh1;
+	cout << "vis=  ";   cin >> vis;
+	cout << "sh=  ";   cin >> sh;
+	cout << "t=  ";   cin >> t;
+	cout << "vis1=  ";   cin >> vis1;
+	cout << "sh1=  ";   cin >> sh1;
+	if (vis <= vis1 && sh <= sh1) cout << "ohhh myyy. NICE";
+	else  cout << "ERRORRRRR";
+}
+
