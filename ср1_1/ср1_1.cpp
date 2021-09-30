665ef106bcb986d665f8f6e51c978f90958252f4
ShkorDanila <89966813+ShkorDanila@users.noreply.github.com>
2021-09-30 10:16:57 +0300

    feat(T1): done

---
+++ ср1_1/ср1_1.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, c, l;
+    cout << "enter a number :";
+    cin >> a;
+    l = a;
+    int i = 0;
+    do
+    {
+        l /= 10;
+        i++;
+    } while (l > 0);
+    cout << "\n";
+    int f;
+    for (int d = i; d >= 1; d--)
+    {
+
+        f = pow(10, d - 1);
+        c = (a / f) % 10;
+        cout << c << "\t";
+    }
+    return 0;
+}
