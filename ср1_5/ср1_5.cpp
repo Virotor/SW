49acb0bf85e12b3538254026ef452660419c8286
ShkorDanila <89966813+ShkorDanila@users.noreply.github.com>
2021-09-30 10:20:44 +0300

    feat(T5): done

---
+++ ср1_5/ср1_5.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a, i, pass[4];
+    for (i = 0; i < 4; i++)
+    {
+        pass[i] = rand() % 10;
+
+    }
+    cout << "password is : ";
+    for (i = 0; i < 4; i++)
+    {
+        for (a = 0; a < 10; a++)
+        {
+            if (a == pass[i]) cout << a;
+        }
+
+    }
+    return 0;
+}
