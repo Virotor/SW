bfa198dcfe93a7b246bd593a17a39c4531f34513
Andrague <90007836+Andrague@users.noreply.github.com>
2021-09-30 11:07:38 +0300

    Задание 2

---
+++ Source 2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Input number of day: ";
+	swith(n)
+	{
+		case 1:
+		{
+			Cout << "Monday";
+			break;
+		}
+		case 2: {
+			Cout << "Tuesday";
+			break;
+		}
+		case 3:
+		{
+			Cout << "Wednesday";
+			break;
+		}
+		case 4:
+		{
+			Cout << "Thursday";
+			break;
+		}
+		case 5:
+		{
+			Cout << "Friday";
+			break;
+		}
+		case 6:
+		{
+			Cout << "Saturday";
+			break;
+		}
+		case 7:
+		{
+			Cout << "Sunday";
+			break;
+		}
+	}
+
+}
