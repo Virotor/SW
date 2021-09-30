c2bb566d75e39907f7edabed85424d9cd8532595
B0r1Koff <vborikov2607@gmail.com>
2021-09-30 20:35:00 +0300

    Update Source.cpp

--- Samosta po TRPO/Samosta po TRPO/Source.cpp
+++ Samosta po TRPO/Samosta po TRPO/Source.cpp
@@ -26,5 +26,20 @@ int main() {
 		}
 		break;
 	}
+	case 2: {
+		int t;
+		cout << "Введите номер дня недели ";
+		cin >> t;
+		switch (t) {
+		case 1: cout << "Понедельник" << "\n"; break;
+		case 2: cout << "Вторник" << "\n"; break;
+		case 3: cout << "Среда" << "\n"; break;
+		case 4: cout << "Четверг" << "\n"; break;
+		case 5: cout << "Пятница" << "\n"; break;
+		case 6: cout << "Суббота" << "\n"; break;
+		case 7: cout << "Воскресенье" << "\n"; break;
+		}
+		break;
+	}
 	}
 }
