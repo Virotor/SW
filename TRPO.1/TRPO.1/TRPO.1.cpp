fb1476863059566699fc753e284e6c9ee6a5f659
[Nikita] <mikita.verkhavodka@gmail.com>
2021-09-30 10:28:06 +0300

    feat(1):done

---
+++ TRPO.1/TRPO.1/TRPO.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+#include <string>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+    int number, x1, x2, x3;
+    cout << "\nEnter a three-digit number: ";
+    cin >> number;
+    x1 = number / 100;
+    number %= 100;
+    x2 = number / 10;
+    number %= 10;
+    x3 = number;
+    cout << x1 << "\n";
+    cout << x2 << "\n";
+    cout << x3 << "\n";
+
+    return 0;
+}
