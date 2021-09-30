bd9568881054b3c854c04a265bfc413f04f2220a
Andrague <90007836+Andrague@users.noreply.github.com>
2021-09-30 19:48:33 +0300

    Задание 3

---
+++ 3-.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<sstream>
+using namespace std;
+int main()
+{
+	int n, k, N = 0;
+	stringstream ss;
+	cout << "Input n: ";
+	cin >> n;
+	cout << "Input k: ";
+	cin >> k;
+	ss << n;
+	while (N != k)
+	{
+		cout << ss.str()[N];
+		N++;
+	}
+	return 0;
+}
