2b94c743f331e75bcb4c9f072f2036d42ff51ab0
[Nikita] <mikita.verkhavodka@gmail.com>
2021-09-30 09:57:58 +0300

    Task#3
    Task#3

--- TRPOSR/TRPOSR/TRPOSR.cpp
+++ TRPOSR/TRPOSR/TRPOSR.cpp
@@ -1,45 +1,17 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
-    int day;
-    cout << "Введите номер дня: ";
-    cin >> day;
+    int n, k;
+    cout << "Enter a N: ";
+    cin >> n;
+    cout << "Enter a K: ";
+    cin >> k;
 
-    switch (day)
-    {
 
-    case 1:
-        cout << "Понедельник";
-        break;
-
-    case 2:
-        cout << "Вторник";
-        break;
-
-    case 3:
-        cout << "Среда";
-        break;
-
-    case 4:
-        cout << "Четверг";
-        break;
-
-    case 5:
-        cout << "Пятница";
-        break;
-
-    case 6:
-        cout << "Суббота";
-        break;
-
-    case 7:
-        cout << "Воскресенье";
-        break;
-
-    default:
-        cout << "Ошибка";
-    }
+    k = to_string(n).length() - k;
+    n /= pow(10, k);
+    cout << n;
 }
