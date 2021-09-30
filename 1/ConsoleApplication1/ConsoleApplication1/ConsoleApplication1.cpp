98eae43f3cd7c84973f3fd848f0bf0e14452ca89
CannyDemon <ruskevich.ilya@mail.ru>
2021-09-30 22:33:07 +0300

    Задание 1
    Задание 1

---
+++ 1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+#include <clocale>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "russian"); 
+	int a;
+	cout << "Введи число\n";
+	cin >> a;
+	while (true)
+	{
+		cout << a % 10 << endl;
+		a /= 10;
+		if (a == 0) break;
+	}
+
+	return 0;
+}
