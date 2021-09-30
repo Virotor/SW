d15deab876be5f1b5169f3fc9df5188dd340e9a6
CannyDemon <ruskevich.ilya@mail.ru>
2021-09-30 23:59:33 +0300

    Update ConsoleApplication1.cpp

--- 5/5/ConsoleApplication1/ConsoleApplication1.cpp
+++ 5/5/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,7 +7,7 @@ int main()
 	setlocale(LC_ALL, "russian");
 	cout << "Загаданный Пароль:\n";
 	srand(time(0));
-	char pass[5]{ rand() % (57 - 48 + 1) + 48, rand() % (57 - 48 + 1) + 48, rand() % (57 - 48 + 1) + 48, rand() % (57 - 48 + 1) + 48 };
+	char pass[5]{ rand() % 10 + 48, rand() % 10 + 48, rand() % 10 + 48, rand() % 10 + 48 };
 	for (char i = '0'; i <= '9'; ++i)
 		for (char j = '0'; j <= '9'; ++j)
 			for (char k = '0'; k <= '9'; ++k)
