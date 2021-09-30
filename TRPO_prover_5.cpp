67d9d101ddd82b556a712c2d0ff8366e9b1a2999
PetrachkovS <90954204+PetrachkovS@users.noreply.github.com>
2021-09-30 19:56:12 +0300

    Add files via upload
    задание №5 ТРПО проверочная работа № 1

---
+++ TRPO_prover_5.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, x, y, z, i;
+	
+	a = rand() % 11;
+	b = rand() % 11;
+	c = rand() % 11;
+	d = rand() % 11;
+
+	for (x = 0; x <= a;x++) {
+		x = x;
+		if (x == a) {
+			cout << x;
+		}
+	}
+	for (y = 0; y <= b; y++) {
+
+		y = y;
+		if (y == b) {
+			cout << y;
+		}
+	}
+	for (z = 0; z <= c; z++) {
+		z = z;
+		if (z == c) {
+		cout << z;
+	}
+	}
+	for (i = 0; i <= d; i++) {
+		i = i;
+		if (i == d) {
+			cout << i;
+		}
+	}
+
+	
+	
+}
+
