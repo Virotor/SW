fac8140438e84e9981ba2f3fca59c14cacff631d
Franak <frangrandev@gmail.com>
2021-09-30 09:32:34 +0300

    feat (Task 1) : add Task 1

--- SelfWork/SelfWork/SelfWork.cpp
+++ SelfWork/SelfWork/SelfWork.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+void Task1()
+{
+	int num;
+	
+	cout << "Введите число: ";
+	cin >> num;
+	while (num > 0)
+	{
+		int temp = num % 10;
+		cout << temp << endl;
+		num /= 10;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+
+	Task1();
 }
