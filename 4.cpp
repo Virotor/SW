16fb7c785a6c57b028a7fe8e9b1da2a3f9a5a26d
Poluyashkin <91225697+Poluyashkin@users.noreply.github.com>
2021-10-01 00:13:43 +0300

    4

---
+++ 4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, h, a1, b1, min1, min2;
+    cout << "enter a, b, h \n";
+    cin >> a >> b >> h;
+    cout << "enter a, b for a hole \n";
+    cin >> a1 >> b1;
+    if (a > b && a > h){
+        min1 = b;
+        min2 = h;
+    }
+    else if (b > a && b > h){
+        min1 = a;
+        min2 = h;
+    }
+    else if (h > a && h > b){
+        min1 = a;
+        min2 = b;
+    }
+    if (min1 <= a1 && min2 <= b1){
+        cout << "suits";
+    }
+    else if (min1 <= b1 && min2 <= a1){
+        cout << "suits";
+    }
+    else{
+        cout << "doesnt suit";
+    }
+}
