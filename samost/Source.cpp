d26b4e1c48363b41e3a5532c70ec1cc8cb1fe2b8
MatthewK25 <kazachok2004@gmail.com>
2021-09-30 22:54:28 +0300

    init(Task4)

--- samost/Source.cpp
+++ samost/Source.cpp
@@ -65,3 +65,23 @@ void z3() {
 		n = n % (b / 10);
 	}
 }
+void z4() {
+	int holeWidth, holeLength, holeHeight, parWidth, parHeight, parLength;
+	cout << "Enter hole length, width and height\n";
+	cin >> holeHeight >> holeLength >> holeWidth;
+	cout << "Enter par length, width and height\n";
+	cin >> parHeight >> parLength >> parWidth;
+	if ((holeHeight >= parHeight && holeWidth >= parWidth && holeLength >= parLength) ||
+		(holeHeight >= parLength && holeWidth >= parWidth && holeLength >= parHeight) ||
+		(holeHeight >= parWidth && holeWidth >= parHeight && holeLength >= parLength) ||
+		(holeHeight >= parHeight && holeWidth >= parLength && holeLength >= parWidth) ||
+		(holeHeight >= parLength && holeWidth >= parHeight && holeLength >= parLength) ||
+		(holeHeight >= parWidth && holeWidth >= parLength && holeLength >= parHeight))
+	{
+		cout << "\nFITS";
+	}
+	else {
+		cout << "\nDOESN'T FIT";
+	}
+
+}
