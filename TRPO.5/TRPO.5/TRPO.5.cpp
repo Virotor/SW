67cddbd05ec43225c0a39849968a50e992a481af
[Nikita] <mikita.verkhavodka@gmail.com>
2021-09-30 10:54:53 +0300

    feat(5):done

---
+++ TRPO.5/TRPO.5/TRPO.5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+    srand(time(NULL));
+    int password, num1, num2, num3, num4, PsevdoPassword;
+    cout << "\nEnter a password: ";
+    cin >> password;
+    do {
+        num1 = rand() % 10;
+        num2 = rand() % 10;
+        num3 = rand() % 10;
+        num4 = rand() % 10;
+        num1 *= 1000;
+        num2 *= 100;
+        num3 *= 10;
+        PsevdoPassword = num1 + num2 + num3 + num4;
+    } while (PsevdoPassword != password);
+    if (PsevdoPassword < 9)
+        cout << "Success! Your password is " << "000" << PsevdoPassword;
+    else if (PsevdoPassword < 99)
+        cout << "Success! Your password is " << "00" << PsevdoPassword;
+    else if (PsevdoPassword < 999)
+        cout << "Success! Your password is " << "0" << PsevdoPassword;
+    else
+        cout << "Success! Your password is " << PsevdoPassword;
+}
