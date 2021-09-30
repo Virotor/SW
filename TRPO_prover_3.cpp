9c29f68f8d137d9376cc3c6ddee77829ecef51d2
PetrachkovS <90954204+PetrachkovS@users.noreply.github.com>
2021-09-30 19:55:09 +0300

    Add files via upload
    задание №3 ТРПО проверочная работа № 1

---
+++ TRPO_prover_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cout << "write a number" << endl;
+	cin >> n;
+	cout << "write k" << endl;
+	cin >> k;
+	while (k<n) {
+		cout << n % 10 << endl;
+		n /= 10;
+		k *= 10;
+	}
+}
