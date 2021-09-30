d005bf3130299954dc99ff67f1919bb67dbbc8ed
txrch <vladimir.iasienko@mail.ru>
2021-09-30 22:02:32 +0300

    No2

--- trpo/Source.cpp
+++ trpo/Source.cpp
@@ -14,3 +14,21 @@ int main()
 	}
 	return 0;
 }
+
+int zadanie2()
+{
+	int y;
+	cin >> y;
+		switch (y)
+		{
+		case 1: cout << "Понедельник" << endl; break;
+		case 2: cout << "Вторник" << endl; break;
+		case 3: cout << "Среда" << endl; break;
+		case 4: cout << "Четверг" << endl; break;
+		case 5: cout << "Пятница" << endl; break;
+		case 6: cout << "Суббота" << endl; break;
+		case 7: cout << "Воскресенье" << endl; break;
+		default: cout << "Ошибка: неверное число." << endl;
+		}
+	return 0;
+}
