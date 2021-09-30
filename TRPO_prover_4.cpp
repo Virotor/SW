bbc443ed1cc63e6d5223e556b32975b38d8749c5
PetrachkovS <90954204+PetrachkovS@users.noreply.github.com>
2021-09-30 19:55:43 +0300

    Add files via upload
    задание №4 ТРПО проверочная работа № 1

---
+++ TRPO_prover_4.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, z, a, b;
+	cout << "write x" << endl;
+	cin >> x;
+	cout << "write y" << endl;
+	cin >> y;
+	cout << "write z" << endl;
+	cin >> z;
+	cout << "write a" << endl;
+	cin >> a;
+	cout << "write b" << endl;
+	cin >> b;
+	if (x <= a) {
+		if (y <= b) {
+			cout << "proidet" << endl;
+		}
+		else {
+			if (z <= b) {
+				cout << "proidet" << endl;
+			}
+		}
+	}
+	if (x <= b) {
+		if (y <= a) {
+			cout << "proidet" << endl;
+		}
+		else {
+			if (z <= a) {
+				cout << "proidet" << endl;
+			}
+		}
+	}
+	if (y <= a) {
+		if (z <= b) {
+			cout << "proidet" << endl;
+		}
+	}
+	if (y <= b) {
+		if (z <= a) {
+			cout << "proidet" << endl;
+		}
+	}
+
+}
+
