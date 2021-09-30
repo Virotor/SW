a419c0fdcc93ccff366908ce82ee37af9f8b62fd
Danya17Bobrov <danya17bobrov@mail.ru>
2021-09-30 16:21:25 +0300

    feat(Task 2): add Task2

--- САМОСТОЙКА ТРПО/САМОСТОЙКА ТРПО/САМОСТОЙКА ТРПО.cpp
+++ САМОСТОЙКА ТРПО/САМОСТОЙКА ТРПО/САМОСТОЙКА ТРПО.cpp
@@ -23,7 +23,36 @@ int main()
 		x = x - z * pow(10, q - 1);
 		cout << z << endl << endl;
 	}
-
+	int DAY;
+	cout << "ЗАДАНИЕ 2." << endl;
+	cout << "НОМЕР ДНЯ НЕДЕЛЯ:  " << endl;
+	cin >> DAY;
+	switch (DAY)
+	{
+	case 1:
+		cout << "ПОНЕДЕЛЬНИК" << endl << endl;
+		break;
+	case 2:
+		cout << "ВТОРНИК" << endl << endl;
+		break;
+	case 3:
+		cout << "СРЕДА" << endl << endl;
+		break;
+	case 4:
+		cout << "ЧЕТВЕРГ" << endl << endl;
+		break;
+	case 5:
+		cout << "ПЯТНИЦА" << endl << endl;
+		break;
+	case 6:
+		cout << "СУББОТА" << endl << endl;
+		break;
+	case 7:
+		cout << "ВОСКРЕСЕНЬЕ" << endl << endl;
+		break;
+	default:
+		cout << "ПОДУМАЙ ЛУЧШЕ( В НЕДЕЛЕ 7 ДНЕЙ )" << endl << endl;
+	}
 	return 0;
 }
 
