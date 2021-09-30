19964dcd4fb316fda31388b8fb0e61fe1b5d5eb7
CannyDemon <ruskevich.ilya@mail.ru>
2021-09-30 22:34:11 +0300

    Задание 2
    Задание 2

---
+++ 2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+#include <clocale>
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	int n;
+	cout << "Введите день недели" << endl;
+	cin >> n;
+	switch (n) {
+	case 1:
+		cout << "Понедельник" << endl;
+		break;
+	case 2:
+		cout << "Вторник" << endl;
+		break;
+	case 3:
+		cout << "Среда" << endl;
+		break;
+	case 4:
+		cout << "Четверг" << endl;
+		break;
+	case 5:
+		cout << "Пятница" << endl;
+		break;
+	case 6:
+		cout << "Суббота" << endl;
+		break;
+	case 7:
+		cout << "Воскресенье" << endl;
+		break;
+	default:
+		cout << "Такого дня нету";
+	}
+
+	return 0;
+}
