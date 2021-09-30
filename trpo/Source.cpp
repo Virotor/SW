271c1764393f1b47a4d39e071506d81461fd2b59
txrch <vladimir.iasienko@mail.ru>
2021-09-30 21:54:03 +0300

    type (scope) :done : build|docs|feat|fix|perf|refactor|test|init  scope : No3

--- trpo/Source.cpp
+++ trpo/Source.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	int y;
-	cout << "Vvedite nomer dnya nedeli" << endl;
-	cin >> y;
-		switch (y)
+	int n, k, x, i, r;
+	cout << "Vvedite n, k: " << endl;
+	cin >> n >> k;
+	if (n == 0) cout << n; else 
+	{
+		i = 0;
+		n = abs(n);
+		x = n;
+		while (x > 0) 
 		{
-		case 1: cout << "Понедельник" << endl; break;
-		case 2: cout << "Вторник" << endl; break;
-		case 3: cout << "Среда" << endl; break;
-		case 4: cout << "Четверг" << endl; break;
-		case 5: cout << "Пятница" << endl; break;
-		case 6: cout << "Суббота" << endl; break;
-		case 7: cout << "Воскресенье" << endl; break;
-		default: cout << "Ошибка. Неверное число" << endl;
+			i++; x /= 10;
 		}
-	return 0;
+		for (x = 1; x <= k; x++) 
+		{
+			r = pow(10, i - x);
+			cout<<"Result: \n" << n / r;
+			n %= r;
+		}
+	}
+	return(0);
 }
