196b928be50036ecfcdd0e6dd827866a10d695ad
txrch <vladimir.iasienko@mail.ru>
2021-09-30 22:03:55 +0300

    No3

--- trpo/Source.cpp
+++ trpo/Source.cpp
@@ -32,3 +32,27 @@ int zadanie2()
 		}
 	return 0;
 }
+
+int zadanie3()
+{
+	int n, k, x, i, r;
+	cout << "Vvedite n, k: " << endl;
+	cin >> n >> k;
+	if (n == 0) cout << n; else
+	{
+		i = 0;
+		n = abs(n);
+		x = n;
+		while (x > 0)
+		{
+			i++; x /= 10;
+		}
+		for (x = 1; x <= k; x++)
+		{
+			r = pow(10, i - x);
+			cout << n / r;
+			n %= r;
+		}
+	}
+	return(0);
+}
