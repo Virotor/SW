63cf44af4e24ff3e98f53d2dce08f30702f834c3
ShkorDanila <89966813+ShkorDanila@users.noreply.github.com>
2021-09-30 10:19:34 +0300

    feat(T3): done

---
+++ ср1_3/ср1_3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+
+    int b;
+    string a;
+    cout << "Enter n and k\n";
+    cin >> a >> b;
+    if (b > size(a))
+    {
+        cout << "Error";
+        return 1;
+    }
+    a = a.erase(b, size(a));
+    cout << "your new number : " << a;
+
+    return 0;
+}
