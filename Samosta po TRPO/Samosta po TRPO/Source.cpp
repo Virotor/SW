c9af474beca32b566a10e21e4de85701ff218b7d
B0r1Koff <vborikov2607@gmail.com>
2021-09-30 22:42:55 +0300

    Update Source.cpp

--- Samosta po TRPO/Samosta po TRPO/Source.cpp
+++ Samosta po TRPO/Samosta po TRPO/Source.cpp
@@ -69,5 +69,27 @@ int main() {
 		if ((a2 < x2) && (b2 < y2)) cout << "Параллелепипед пройдет в отверстие";
 		else cout << "Параллелепипед не пройдет в отверстие";
 		return 0; } break;
+	case 5: {srand(time(0));
+		int firstDigitOfPassword1;
+			firstDigitOfPassword1=rand()%10;
+			int secondDigitOfPassword1;
+			secondDigitOfPassword1 = rand() % 10;
+			int thirdDigitOfPassword1;
+			thirdDigitOfPassword1 = rand() % 10;
+			int fourthDigitOfPassword1;
+			fourthDigitOfPassword1 = rand() % 10;
+			for (int firstDigitOfPassword2 = 0; firstDigitOfPassword2 < 10; firstDigitOfPassword2++) {
+				for (int secondDigitOfPassword2 = 0; secondDigitOfPassword2 < 10; secondDigitOfPassword2++) {
+					for (int thirdDigitOfPassword2 = 0; thirdDigitOfPassword2 < 10; thirdDigitOfPassword2++) {
+						for (int fourthDigitOfPassword2 = 0; fourthDigitOfPassword2 < 10; fourthDigitOfPassword2++) {
+							if (firstDigitOfPassword1 == firstDigitOfPassword2 and secondDigitOfPassword1 == secondDigitOfPassword2 and thirdDigitOfPassword1 == thirdDigitOfPassword2 and fourthDigitOfPassword1 == fourthDigitOfPassword2) {
+								cout << "password: " << firstDigitOfPassword2 << secondDigitOfPassword2 << thirdDigitOfPassword2 << fourthDigitOfPassword2; break;
+							}
+						}
+					}
+				}
+			}
+			return 0;
+	}
 	}
 }
