c1fbcc6da2ae5dcf181309ca5c9119fe18cb4f36
Bukhta <buhta.03@mail.ru>
2021-09-30 16:30:10 +0300

    Update ТРПО 29,09,21.cpp

--- ТРПО 29,09,21/ТРПО 29,09,21/ТРПО 29,09,21.cpp
+++ ТРПО 29,09,21/ТРПО 29,09,21/ТРПО 29,09,21.cpp
@@ -10,5 +10,31 @@ int main() {
 		cout << x % 10 << endl;
 		x /= 10;
 	}
+	int a;
+	cout << "Введите номер дня недели "; cin >> a;
+	switch (a) {
+	case 1:
+		cout << "Понедельник" << endl;
+		break;
+	case 2:
+		cout << "Вторник" << endl;
+		break;
+	case 3:
+		cout << "Среда" << endl;
+		break;
+	case 4:
+		cout << "Четверг" << endl;
+		break;
+	case 5:
+		cout << "Пятница" << endl;
+		break;
+	case 6:
+		cout << "Суббота" << endl;
+		break;
+	case 7:
+		cout << "Воскресенье" << endl;
+		break;
+	}
+
 	return 0;
 }
