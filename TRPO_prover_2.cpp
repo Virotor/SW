b85b188dfbcab73ac8e3e1ef4a28d47460206ac1
PetrachkovS <90954204+PetrachkovS@users.noreply.github.com>
2021-09-30 19:54:46 +0300

    Add files via upload
    задание №2 ТРПО проверочная работа № 1

---
+++ TRPO_prover_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "write a number from 1 to 7" << endl;
+	cin >> n;
+	switch (n) {
+	case 1:
+		cout << "monday" << endl;
+		break;
+	case 2:
+		cout << "thuseday" << endl;
+		break;
+	case 3:
+		cout << "wednesday" << endl;
+		break;
+	case 4:
+		cout << "thursday" << endl;
+		break;
+	case 5:
+		cout << "friday" << endl;
+		break;
+	case 6:
+		cout << "saturday" << endl;
+		break;
+	case 7:
+		cout << "sunday" << endl;
+		break;
+	default:cout << "error" << endl;
+	}
+}
