45a6570843921dda57d5213ec67134448b301f8c
krasmaria <krasnogolovmaria@gmail.com>
2021-09-30 10:00:59 +0300

    praktika

---
+++ sm1/sm1/sm1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	int n, c, x, i,b;
+	cout << "Vvedite chislo" << endl;
+	cin >> x;
+	x = abs(x);
+	b = x;
+	n = 1;
+	while ((b /= 10) > 0) n++;
+	for (i = n; i > 0;i--) {
+		c = x / pow(10,i-1);
+		cout << c<<endl;
+		x =x-c* pow(10, i-1);
+	}
+	return 0;
+}
+
+
+
---
+++ sm3/sm3/sm3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<math.h>
+#include<locale.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int k, n, z, c, i, v;
+	
+	while (true) {
+		cout << "Введите 2 числа, первое больше второго" << endl;
+		cin >> n >> k;
+		if (n > k) {
+			z = 1;
+			v = n;
+			while ((v /= 10) > 0) z++;
+			c = z - k;
+			i = n / pow(10, c);
+			cout << i << endl;
+			break;
+		}
+		else cout << "Числа не подходят" << endl;
+		
+
+	}
+	return 0;
+}
