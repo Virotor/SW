82458f9e748181a3c602989222c2e282eb2c165c
MatthewK25 <kazachok2004@gmail.com>
2021-09-30 10:09:30 +0300

    init(T5)

--- samost/Source.cpp
+++ samost/Source.cpp
@@ -1,23 +1,33 @@
 #include <iostream>;
+#include <locale.h>;
+#include <sstream>;
 using namespace std;
 int main() {
-	int holeWidth, holeLength, holeHeight, parWidth, parHeight, parLength;
-	cout << "Enter hole length, width and height\n";
-	cin >> holeHeight >> holeLength >> holeWidth;
-	cout << "Enter par length, width and height\n";
-	cin >> parHeight >> parLength >> parWidth;
-	if ((holeHeight >= parHeight && holeWidth >= parWidth && holeLength >= parLength) ||
-		(holeHeight >= parLength && holeWidth >= parWidth && holeLength >= parHeight) ||
-		(holeHeight >= parWidth && holeWidth >= parHeight && holeLength >= parLength) ||
-		(holeHeight >= parHeight && holeWidth >= parLength && holeLength >= parWidth) ||
-		(holeHeight >= parLength && holeWidth >= parHeight && holeLength >= parLength)||
-	    (holeHeight >= parWidth && holeWidth >= parLength && holeLength >= parHeight))
-	{
-		cout << "\nFITS";
+	string s;
+	char symbol1, symbol2, symbol3, symbol4;
+	symbol1 = rand() % 10 + 48;
+	symbol2 = rand() % 10 + 48;
+	symbol3 = rand() % 10 + 48;
+	symbol4 = rand() % 10 + 48;
+	char arr[4] = { symbol1, symbol2, symbol3, symbol4 };
+	s = string(arr);
+	int a;
+	int x = 0;
+	stringstream ss;
+	ss << s;
+	ss >> a;
+	cout << a << endl;
+	int array[4];
+	for (int i = 3; i >= 0; i--) {
+		array[i] = a % 10;
+		a /= 10;
 	}
-	else{
-		cout << "\nDOESN'T FIT";
+	cout << "Password:";
+	for (int i = 0; i <= 3; i++) {
+		for (x; x != array[i]; x++) {
+		}
+		cout << x;
 	}
-	
+
 	return 0;
 }
