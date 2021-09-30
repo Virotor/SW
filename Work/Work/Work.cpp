60973de8dfd051c54144ce7823b098c1c61b873c
D1le1 <egor20031500@gmail.com>
2021-09-30 09:56:47 +0300

    feat (Task#2) : add Task#2

--- Work/Work/Work.cpp
+++ Work/Work/Work.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int task1();
+int task2();
 
 int main()
 {
 	setlocale(LC_ALL,"rus");
 	task1();
+	task2();
 	return 0;
 }
 
@@ -26,5 +28,26 @@ int task1()
 	}
 	for (int i = digit.length(); i >= 0; i--)
 		cout << digit[i] << endl;
+	return 0;
+}
+int task2()
+{
+	cout << "\n\tЗадание 2\n";
+	int day;
+	do{
+		cout << "Введите номер дня недели: ";
+		cin >> day;
+		switch (day)
+		{
+		case 1: cout << "Понедельник" << endl; break;
+		case 2: cout << "Вторник" << endl; break;
+		case 3: cout << "Среда" << endl; break;
+		case 4: cout << "Четверг" << endl; break;
+		case 5: cout << "Пятница" << endl; break;
+		case 6: cout << "Суббота" << endl; break;
+		case 7: cout << "Воскресенье" << endl; break;
+		default: cout << "Такого дня нет!!!" << endl; break;
+		}
+	} while (day > 7 || day < 1);
 	return 0;
 }
