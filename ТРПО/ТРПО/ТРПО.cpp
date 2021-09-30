97199b11488d532d9573cb344eaad96102935f58
Bukhta <buhta.03@mail.ru>
2021-09-30 16:40:42 +0300

    Feat (Task 2): Задание 2

--- ТРПО/ТРПО/ТРПО.cpp
+++ ТРПО/ТРПО/ТРПО.cpp
@@ -10,5 +10,35 @@ int main() {
 		cout << x % 10 << endl;
 		x /= 10;
 	}
+	int a;
+	cout << "Введите номер дня недели "; cin >> a;
+	switch (a) {
+	case 1:
+		cout << "Понедельник" << endl;
+		break;
+	case 2:
+		cout << "Вторник" << endl;
+		break;
+	case 3:
+		cout << "Среда" << endl;
+		break;
+	case 4:
+		cout << "Четверг" << endl;
+		break;
+	case 5:
+		cout << "Пятница" << endl;
+		break;
+	case 6:
+		cout << "Суббота" << endl;
+		break;
+	case 7:
+		cout << "Воскресенье" << endl;
+		break;
+	}
+	if (a > 7 && a < 1)
+		cout << "Нет такого дня недели\n";
+
+
+
 	return 0;
 }
