298b8d20ca8910c3bc73008559cf91bdad6ec6e1
MatthewK25 <kazachok2004@gmail.com>
2021-09-30 22:52:51 +0300

    init(Task3)

--- samost/Source.cpp
+++ samost/Source.cpp
@@ -47,3 +47,21 @@ void z2()
 	default: cout << "Error";
 	}
 }
+void z3() {
+	cout << "Enter n and k ";
+	int n, a, b, k, g, h = 1;
+	cin >> n;
+	a = n;
+	for (a; a > 10; a /= 10) {
+		h++;
+	}
+	cin >> k;
+	int e = h - k;
+	for (h; h > e; h--) {
+		b = pow(10, h);
+		g = n;
+		g = g / (b / 10);
+		cout << g;
+		n = n % (b / 10);
+	}
+}
