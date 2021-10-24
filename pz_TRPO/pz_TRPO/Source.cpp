05c84264742055b447b7ad34cc8db2864385ec28
ArtyrRoshchupkin <artur.roshchupkin.03@mail.ru>
2021-10-24 22:15:36 +0300

    pz4_task2

--- pz_TRPO/pz_TRPO/Source.cpp
+++ pz_TRPO/pz_TRPO/Source.cpp
@@ -6,9 +6,11 @@
 using namespace std;
 
 void task1();
+void task2();
 
 int main() {
 	task1();
+	task2();
 	system("pause");
 	return 0;
 }
@@ -50,6 +52,38 @@ void task1() {
 			}
 
 			delete[]matrix;
+			system("pause");
+		}
+	}
+}
+
+void task2() {
+	int size, min, max, interval;
+	double sum = 0, srednee;
+	cout << endl << "Task number 2  " << endl;
+	cout << "Enter the size of the array  "; cin >> size;
+	if (size <= 0) cout << "Invalid input  \n";
+	else {
+		cout << "Enter minimum element : "; cin >> min;
+		cout << "Enter the maximum element : "; cin >> max;
+
+		if (max < min)cout << "Invalid input ";
+		else {
+
+			int* arr = new int[size];
+
+			interval = (max - min + 1);
+			for (int i = 0; i < size; i++) {
+				arr[i] = rand() % interval + min;
+				cout << arr[i] << " ";
+				sum += arr[i];
+			}
+
+			srednee = sum / size;
+			cout << "\nThe sum of the array elements is : " << sum;
+			cout << "\nAverage value of array elements  = " << srednee << endl;
+
+			delete[]arr;
 			system("pause");
 		}
 	}
