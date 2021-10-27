9117907cfe35e7cea389528462d70abc00a24887
Bukhta <buhta.03@mail.ru>
2021-10-27 23:44:52 +0300

    init(Task#3):Task 3

--- Task3/Task 3/Task 3.cpp
+++ Task3/Task 3/Task 3.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void inputMassive(int* arr, int size)
 {
 	for (int i = 0; i < size; i++) {
-		arr[i] = rand() % 21 - 10;
+		arr[i] = rand() % 4;
 	}
 
 }
@@ -20,24 +20,31 @@ void outputMassive(int* arr, int size)
 	}
 }
 
-void DifferenceNumber() {
+void taskBiggestLength() {
 	srand(time(NULL));
 	int size;
 
-	cout << "Enter size of massive:  ";
-	cin >> size;
+	cout << "Enter size of massive "; cin >> size;
 	int* arr = new int[size];
 	inputMassive(arr, size);
-
 	outputMassive(arr, size);
 	cout << endl;
-	for (int i = 2; i < size; i++) {
-		if (arr[i] < arr[i - 2] - arr[i - 1]) cout << setw(6) << arr[i];
+
+	int length = 1, big_length = 1, number;
+	number = arr[0];
+	for (int i = 1; i < size; i++) {
+		if (number == arr[i]) length++;
+		else {
+			big_length = max(length, big_length);
+			length = 1;
+			number = arr[i];
+		}
 	}
+	cout << big_length;
 	delete[]arr;
 }
 
 int main()
 {
-	DifferenceNumber();
+	taskBiggestLength();
 }
