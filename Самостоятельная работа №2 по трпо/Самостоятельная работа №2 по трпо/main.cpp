b61e4941f3ebbb85b47ab0711e14a72f7d8442e6
lesik2 <lesikpuhalskij@gmail.com>
2021-10-27 22:04:37 +0300

    задание 3

--- Самостоятельная работа №2 по трпо/Самостоятельная работа №2 по трпо/main.cpp
+++ Самостоятельная работа №2 по трпо/Самостоятельная работа №2 по трпо/main.cpp
@@ -4,6 +4,8 @@ void Fill_Array(int*, int);
 void Show_Array(int*, int);
 void Serching_index(int*, int,int);
 void Less_diference(int*, int);
+int Max_length(int*, int);
+void Show_Array2(int*, int);
 int main()
 {
 	setlocale(LC_ALL, "RUS");
@@ -15,13 +17,18 @@ int main()
 	cout << "enter number to search the index";
 	cin >> number;
 	int* arr = new int[size_of_array];
+	int* arr2 = new int[size_of_array];
 	Fill_Array(arr, size_of_array);
 	Show_Array(arr, size_of_array);
 	Serching_index(arr, size_of_array, number);
 	Less_diference(arr, size_of_array);
 	cout << endl;
+	Show_Array2(arr2, size_of_array);
+
+	cout << "Max = " << Max_length(arr2, size_of_array);
 
 	delete[]arr;
+	delete[]arr2;
 
 }
 void Fill_Array(int* arr, int size_of_array)
@@ -65,5 +72,34 @@ void Less_diference(int* arr, int size_of_array)
 		{
 			cout << arr[i] << "\t";
 		}
+	}
+}
+int Max_length(int* arr2, int size_of_array)
+{
+	int n = 1;
+	int max = 0;
+	for (int i = 0; i < size_of_array - 1; i++)
+	{
+		if (arr2[i] == arr2[i + 1])
+		{
+			n++;
+		}
+		else
+		{
+			n = 1;
+		}
+		if (n > max)
+		{
+			max = n;
+		}
+	}
+	return max;
+}
+void Show_Array2(int* arr2, int size_of_array)
+{
+	for (int i = 0; i < size_of_array; i++)
+	{
+		cout << "arr2[" << i + 1 << "]=";
+		cin >> arr2[i];
 	}
 }
