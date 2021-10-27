03405fc0b7c0d87a28e025b7d68b99653b836686
MariaBrichkovskaya <brichkovskayam@gmail.com>
2021-10-27 21:07:30 +0300

    Create SW2.cpp

---
+++ SW2/SW2/SW2.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+using namespace std;
+int* initArray(int length);
+void printArray(int* array, int length);
+int positionOfTheNumber(int* array, int length, int number);
+
+
+int main()
+{
+    int length,number;
+    cout << "Enter length of the array : ";
+    cin >> length;
+    srand(time(NULL));
+    int* array = initArray(length);
+    printArray(array, length);
+    cout << "Enter number : ";
+    cin >> number;
+    cout <<"Position of this number is "<< positionOfTheNumber(array, length, number) << endl;
+	system("pause");
+	return 0;
+}
+int* initArray(int length)
+{
+    int* array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand() % 20 - 10;
+    }
+    return array;
+}
+
+void printArray(int* array, int length)
+{
+    for (int j = 0; j < length; j++)
+    {
+        cout << array[j] << " ";
+    }
+    cout << endl;
+}
+
+int positionOfTheNumber(int* array, int length, int number)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i]==number)
+        {
+            return i;
+        }
+    }
+}
