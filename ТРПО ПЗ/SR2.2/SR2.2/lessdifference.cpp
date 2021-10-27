1e03514a74da7546f688b7e23b2abaca75e2abb6
danik440 <v.danik03@yandex.by>
2021-10-27 23:52:47 +0300

    Create lessdifference.cpp

---
+++ ТРПО ПЗ/SR2.2/SR2.2/lessdifference.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void diffOfPrevNum() {
+	int sizeOfArr;
+	cout << "¬ведите длину массива: "; 
+	cin >> sizeOfArr;
+	int* arr = new int[sizeOfArr];
+
+	for (int j = 0; j < sizeOfArr; j++)
+	{
+		arr[j] = rand() % 20;
+		cout << arr[j] << ", ";
+	}
+	cout << endl;
+
+	int examination = 0;
+	for (int i = 0; i <= sizeOfArr - 3; i++) {
+		if ((arr[i] - arr[i + 1]) > arr[i + 2]) {
+			cout << " Ч> " << arr[i + 2] << endl;
+			examination++;
+		}
+	}
+
+	delete[] arr;
+
+}
