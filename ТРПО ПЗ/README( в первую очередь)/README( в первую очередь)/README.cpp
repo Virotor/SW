f235142e95984726af8a1d1fdb97d567801827ba
danik440 <v.danik03@yandex.by>
2021-10-28 00:49:14 +0300

    Update README.cpp

--- ТРПО ПЗ/README( в первую очередь)/README( в первую очередь)/README.cpp
+++ ТРПО ПЗ/README( в первую очередь)/README( в первую очередь)/README.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	setlocale(LC_ALL, "rus");
+	cout << "я опять по тупому сделал коммит :/";
 
-я опять по тупому сделал коммит:/
+	return 0;
+}
