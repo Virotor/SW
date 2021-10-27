40c727801abb2722c78297419fdf54191d8f5d94
lesik2 <lesikpuhalskij@gmail.com>
2021-10-27 21:44:35 +0300

    Create main.cpp

---
+++ Самостоятельная работа №2 по трпо/Самостоятельная работа №2 по трпо/main.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+void Fill_Array(int*, int);
+void Show_Array(int*, int);
+void Serching_index(int*, int,int);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	srand(time(NULL));
+	int size_of_array;
+	cout << "enter size of array" << "\t";
+	cin >> size_of_array;
+	int number;
+	cout << "enter number to search the index";
+	cin >> number;
+	int* arr = new int[size_of_array];
+	Fill_Array(arr, size_of_array);
+	Show_Array(arr, size_of_array);
+	Serching_index(arr, size_of_array, number);
+
+	delete[]arr;
+
+}
+void Fill_Array(int* arr, int size_of_array)
+{
+	for (int i = 0; i < size_of_array; i++)
+	{
+		arr[i] = rand() % 20-10;
+	}
+}
+void Show_Array(int* arr, int size_of_array)
+{
+	
+	for (int i = 0; i < size_of_array; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Serching_index(int* arr, int size_of_array,int number)
+{
+	bool flag = true;
+	for (int i = 0; i < size_of_array; i++)
+	{
+		if (arr[i] == number)
+		{
+			cout << "index = " << i << endl;
+			flag = false;
+			break;
+		}
+	}
+	if (flag==true)
+	{
+		cout << "there is no such number" << endl;
+	}
+}
