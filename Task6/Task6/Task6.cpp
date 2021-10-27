1af090e03ac2e0db8c1f133765c090a54452b84e
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2021-10-27 18:37:51 +0300

    feat 2
    Added task 2

--- Task6/Task6/Task6.cpp
+++ Task6/Task6/Task6.cpp
@@ -5,11 +5,14 @@
 using namespace std;
 
 void PositionOfFirstEqualNumber();
+//MagicNumbers - числа, которые меньше разности двух предыдущих в массиве. Как их ещё лаконично назвать?
+void TaskWithMagicNumbers();
 
 int* CreateArray(int);
 void SetArray(int, int*);
 void PrintArray(int, int*);
 void SearchEqualElement(int, int, int*);
+string SearchMagicNumbers(int, int*);
 
 int main()
 {
@@ -26,7 +29,7 @@ int main()
 		PositionOfFirstEqualNumber();
 		break;
 	case 2:
-
+		TaskWithMagicNumbers();
 		break;
 	case 3:
 
@@ -56,6 +59,26 @@ void PositionOfFirstEqualNumber()
 	cin >> element;
 
 	SearchEqualElement(element, size, array);
+
+	delete[]array;
+}
+
+void TaskWithMagicNumbers()
+{
+	int size;
+
+	do
+	{
+		cout << "Введите размер массива (не меньше трёх элементов) ";
+		cin >> size;
+		size = abs(size);
+	} while (size < 3);
+
+	int* array = CreateArray(size);
+	SetArray(size, array);
+	PrintArray(size, array);
+
+	cout << SearchMagicNumbers(size, array);
 
 	delete[]array;
 }
@@ -106,4 +129,25 @@ void SearchEqualElement(int element, int size, int* array)
 	{
 		cout << "В данном массиве нет такого элемента";
 	}
+}
+
+string SearchMagicNumbers(int size, int* array)
+{
+	string magicNumbers;
+	int numberToCheck, numberToCompare;
+	bool hasNumbers = false;
+
+	for (int i = 2; i < size; i++)
+	{
+		numberToCompare = array[i - 1] - array[i - 2];
+		numberToCheck = array[i];
+		if (numberToCheck < numberToCompare)
+		{
+			magicNumbers += to_string(numberToCheck);
+			magicNumbers += " ";
+			hasNumbers = true;
+		}
+	}
+
+	return hasNumbers ? magicNumbers : "В массиве нет таких чисел";
 }
