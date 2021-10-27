fea5d4072c12ede711e52f66e8b2cc756b3e6eba
lesik2 <lesikpuhalskij@gmail.com>
2021-10-27 22:10:23 +0300

    задание 4

--- Самостоятельная работа №2 по трпо/Самостоятельная работа №2 по трпо/main.cpp
+++ Самостоятельная работа №2 по трпо/Самостоятельная работа №2 по трпо/main.cpp
@@ -6,6 +6,10 @@ void Serching_index(int*, int,int);
 void Less_diference(int*, int);
 int Max_length(int*, int);
 void Show_Array2(int*, int);
+void Fill_Matrix(int**, int);
+void Show_Matrix(int**, int);
+void Transp_glav(int**, int);
+void Transp_poboch(int**, int);
 int main()
 {
 	setlocale(LC_ALL, "RUS");
@@ -26,9 +30,29 @@ int main()
 	Show_Array2(arr2, size_of_array);
 
 	cout << "Max = " << Max_length(arr2, size_of_array);
+	int rows;
+	cout << endl;
+	cout << "number of rows = ";
+	cin >> rows;
+	int** arr3 = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr3[i] = new int[rows];
+	}
+	Fill_Matrix(arr3, rows);
+	Show_Matrix(arr3, rows);
+	cout << endl;
+	Transp_glav(arr3, rows);
+	cout << endl;
+	Transp_poboch(arr3, rows);
 
 	delete[]arr;
 	delete[]arr2;
+	for (int i = 0; i < rows; i++)
+	{
+		delete[i]arr3;
+	}
+	delete[]arr3;
 
 }
 void Fill_Array(int* arr, int size_of_array)
@@ -101,5 +125,48 @@ void Show_Array2(int* arr2, int size_of_array)
 	{
 		cout << "arr2[" << i + 1 << "]=";
 		cin >> arr2[i];
+	}
+}
+void Fill_Matrix(int** arr3, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			arr3[i][j] = rand() % 20 - 10;
+		}
+	}
+}
+void Show_Matrix(int** arr3, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			cout << arr3[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+void Transp_glav(int** arr3, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			cout << arr3[j][i] << "\t";
+		}
+		cout << endl;
+	}
+}
+void Transp_poboch(int** arr3, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			cout << arr3[rows - 1 - j][rows - 1 - i] << "\t";
+		}
+		cout << endl;
 	}
 }
