d9f6505b306a7e00a046f6bdede1a0d8aa127698
AndreiZastupnevich <andreizastupnevich@gmail.com>
2021-10-27 22:16:56 +0300

    feat(T2): done

---
+++ sr2_t2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void lessThanPrevDif(int arr[], int n) {
+    cout << "\nОтвет: ";
+    bool isDigitHere = false;
+    for (int i = 2; i < n; i++) {
+        if (arr[i] < arr[i - 2] - arr[i - 1]) {
+            isDigitHere = true;
+            cout << arr[i] << " ";
+        }
+    }
+    if (!isDigitHere) cout << "таких чисел нет!!!";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) arr[i] = 50 - rand() % 101;
+    cout << "Итоговый массив: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    lessThanPrevDif(arr, n);
+    delete[]arr;
+    return 0;
+}
