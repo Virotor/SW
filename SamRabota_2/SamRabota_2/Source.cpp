a1773e4ceb9534b49b1e7dbb33902c1d47f17d8e
ArtyrRoshchupkin <artur.roshchupkin.03@mail.ru>
2021-10-27 21:39:50 +0300

    task_2

--- SamRabota_2/SamRabota_2/Source.cpp
+++ SamRabota_2/SamRabota_2/Source.cpp
@@ -1,3 +1,5 @@
+//TASK_1
+
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
@@ -41,4 +43,51 @@ int main()
 {
 	taskIndexFirstElement();
 }
+
+//TASK_2
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+void inputMassive(int* arr, int size)
+{
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 21 - 10;
+	}
+
+}
+
+void outputMassive(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << setw(5) << arr[i];
+	}
+}
+
+void taskNumberAndDifference() {
+	srand(time(NULL));
+	int size;
+
+	cout << "Enter size of massive "; cin >> size;
+	int* arr = new int[size];
+	inputMassive(arr, size);
+
+	outputMassive(arr, size);
+	cout << endl;
+	for (int i = 2; i < size; i++) {
+		if (arr[i] < arr[i - 2] - arr[i - 1]) cout << setw(6) << arr[i];
+	}
+	delete[]arr;
+}
+
+int main()
+{
+	taskNumberAndDifference();
+}
+
+//TASK_3
 
