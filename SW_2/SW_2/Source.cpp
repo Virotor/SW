2c86a74b63001e41a3450fd70b6e9ce9737b4d5f
SaveliyKomlenok <skomlenok363@gmail.com>
2021-10-27 22:49:22 +0300

    Type: init; scope: Task 2;
    Short summary: Task 2 completed;

--- SW_2/SW_2/Source.cpp
+++ SW_2/SW_2/Source.cpp
@@ -35,6 +35,28 @@ void positionWithdrawal(int* positionArray, int size, int max, int min) {
 	delete[] positionArray;
 }
 
+void differenceBetweenTwoPrevios(int* previosArray, int size, int max, int min) {
+	int j = 0;
+	for (int i = 2; i < size; i++) {
+		if (previosArray[i - 2] - previosArray[i - 1] > previosArray[i]) {
+			j++;
+		}
+	}
+	if (j > 0) {
+		cout << endl << " Numbers that are less than the difference between the two previos: ";
+		for (int i = 2; i < size; i++) {
+			if (previosArray[i - 2] - previosArray[i - 1] > previosArray[i]) {
+				cout << previosArray[i] << " ";
+			}
+		}
+	}
+	else {
+		cout << endl << " There is no number that is less than the previous two " << endl;
+	}
+	cout << endl;
+	delete[] previosArray;
+}
+
 int main() {
 	srand(time(0));
 	int size, min, max, rows, cols, minMatrix, maxMatrix;
@@ -48,6 +70,23 @@ int main() {
 		inputArray(positionArray, size, max, min);
 		printArray(positionArray, size);
 		positionWithdrawal(positionArray, size, max, min);
+	}
+	else if (size < 0) {
+		cout << " Size of massive must be natural number " << endl;
+	}
+	else if (min > max) {
+		cout << " Minimum more than maximum " << endl;
+	}
+
+	cout << " Task 2 " << endl;
+	cout << " Input size of 'previos' massive: "; cin >> size;
+	cout << " Input minimum of your random range: "; cin >> min;
+	cout << " Input maximum of your random range: "; cin >> max;
+	if (size > 0) {
+		int* previosArray = new int[size];
+		inputArray(previosArray, size, max, min);
+		printArray(previosArray, size);
+		differenceBetweenTwoPrevios(previosArray, size, max, min);
 	}
 	else if (size < 0) {
 		cout << " Size of massive must be natural number " << endl;
