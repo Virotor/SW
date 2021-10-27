a778f0b695c94da4db7320fc72436543d30e5aa8
VladStavrov <vlad.stavrov2004.stavrov@mail.ru>
2021-10-27 22:07:23 +0300

    Samost№2Task2

--- Samost№2Task1/Samost№2Task1/Source.cpp
+++ Samost№2Task1/Samost№2Task1/Source.cpp
@@ -8,15 +8,56 @@ void InputOne_dimensionalArray(int, int*);
 void ShowOne_dimensionalArray(int, int*);
 void searchSubstring();
 void ShowPosition();
+void searchNumbers();
 int main() {
 
 
-
-
+	
+	searchNumbers();
 
 	system("pause");
 
 	return 0;
+}
+
+void RandomOne_dimensionalArray(int sizeArray, int* Array) {
+	int i;
+	srand(time(NULL));
+	int minRandom, maxRandom;
+	cout << "Minimum random number =";
+	cin >> minRandom;
+	cout << endl;
+	cout << "Maximum random number =";
+	cin >> maxRandom;
+	cout << endl;
+	int g;
+	g = maxRandom - minRandom + 1;
+	for (i = 0; i < sizeArray; i++)
+	{
+		Array[i] = rand() % g + minRandom;
+
+	}
+
+
+}
+void InputOne_dimensionalArray(int sizeArray, int* Array) {
+	int i;
+	cout << "Please, enter " << sizeArray << " numbers" << endl;
+	for (i = 0; i < sizeArray; i++)
+	{
+		cin >> Array[i];
+
+	}
+}
+void ShowOne_dimensionalArray(int sizeArray, int* Array) {
+	int i;
+	for (i = 0; i < sizeArray; i++)
+	{
+		cout << Array[i] << " ";
+
+	}
+
+	cout << endl;
 }
 void ShowPosition() {
 	int sizeArray;
@@ -99,42 +140,44 @@ void searchSubstring() {
 	if (flag1 == false)
 		cout << "There is no such substring in the string" << endl;
 };
-void RandomOne_dimensionalArray(int sizeArray, int* Array) {
-	int i;
-	srand(time(NULL));
-	int minRandom, maxRandom;
-	cout << "Minimum random number =";
-	cin >> minRandom;
-	cout << endl;
-	cout << "Maximum random number =";
-	cin >> maxRandom;
-	cout << endl;
-	int g;
-	g = maxRandom - minRandom + 1;
-	for (i = 0; i < sizeArray; i++)
+void searchNumbers(){
+	int sizeArray;
+	cout << "Enter the size>2 of the array" << endl;//i didn't check to save time
+	cin >> sizeArray;
+	int* array = new int[sizeArray];
+	cout << "Choose the method to fill the array" << endl;
+	cout << "1.Random(default)" << endl << "2.From the keyboard" << endl;
+	char FillingMethod;
+	FillingMethod = _getch();
+	cout << FillingMethod << endl;
+	switch (FillingMethod)
 	{
-		Array[i] = rand() % g + minRandom;
+	case '1':default: {
+		RandomOne_dimensionalArray(sizeArray, array);
+
+		break; }
+	case'2': {
+		InputOne_dimensionalArray(sizeArray, array);
+		break;
+	}
+	}
+	cout << "Array:" << endl;
+	ShowOne_dimensionalArray(sizeArray, array);
+	int i, difference;
+	bool flag = false;
+	cout << "Result:" << endl;
+	for (i = 2; i < sizeArray; i++) {
+		difference = array[i - 2] - array[i - 1];
+		if (array[i] < difference) {
+			flag = true;
+			cout << array[i] << " ";
+		}
+
 
 	}
-
-
-}
-void InputOne_dimensionalArray(int sizeArray, int* Array) {
-	int i;
-	cout << "Please, enter " << sizeArray << " numbers" << endl;
-	for (i = 0; i < sizeArray; i++)
-	{
-		cin >> Array[i];
-
-	}
-}
-void ShowOne_dimensionalArray(int sizeArray, int* Array) {
-	int i;
-	for (i = 0; i < sizeArray; i++)
-	{
-		cout << Array[i] << " ";
-
-	}
-
 	cout << endl;
+	if (flag == false)
+		cout << "There is no such number in the array" << endl;
+
+	delete[]array;
 }
