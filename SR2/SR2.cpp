1ea923b3fb304283bf71103ac2e368855d8ad61a
Linkler1864 <ivanovwrw@gmail.com>
2021-10-27 16:47:56 +0300

    Feat (T2) : Task2

--- SR2/SR2.cpp
+++ SR2/SR2.cpp
@@ -45,6 +45,28 @@ void Element_position()
 	delete []array;
 }
 
+void Numbers_search()
+{
+	int size, *array, flag=0;
+	cout << "Enter size of array" << endl;
+	cin >> size;
+	array = Array_create(size);
+	Array_print(array, size);
+	if (size > 2)
+		for (int i = 2;i < size;i++)
+		{
+			if (array[i] < array[i - 2] - array[i - 1])
+			{ 
+				cout << array[i] << ' ';
+				flag = 1;
+			}
+		}
+	else cout << "No such numbers"<<endl;
+	if (flag==0)
+		cout << "No such numbers" << endl;
+	delete []array;
+}
+
 int main()
 {
 	int task_number;
@@ -54,6 +76,9 @@ int main()
 	{
 	case 1:
 		Element_position();
+		break;
+	case 2:
+		Numbers_search();
 		break;
 	}
 }
