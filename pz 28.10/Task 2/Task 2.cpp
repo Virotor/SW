ed894534ff813b5394048a89d97ef1b42446808f
CannyDemon <ruskevich.ilya@mail.ru>
2021-10-28 21:01:26 +0300

    init: T2
    ia ne razymiu kak nazivat commit

---
+++ pz 28.10/Task 2/Task 2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int* Arr;
+    int size;
+    cout << "Введите размер масива\n";
+    cin >> size;
+    Arr = new int[size]; 
+    srand(time(NULL));
+    int num;
+    for (int i = 0; i < size; i++)
+    {
+        num = rand() % 99 + 1; 
+        Arr[i] = num;
+    }
+    for (int i = 0; i < size; i++) { cout << Arr[i] << " "; }
+    cout << endl;
+    cout << "Поиск номера\n";
+    for (int i = 2; i < size; i++) {
+        if (Arr[i] < Arr[i - 2] - Arr[i - 1]) {
+            cout << "number = " << Arr[i] << "\n";
+           
+        }
+       
+    }
+
+    
+    delete[]Arr;
+
+    system("pause");
+}
