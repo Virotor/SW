81393a6337ca71b50a54ef4ded5a26c29d2265b3
PetrachkovS <thegreateandpouwerful@gmail.com>
2021-10-28 23:27:42 +0300

    задания 1 2 3 4 С.Р. 2

---
+++ C.P._Task_2(4).cpp
@@ -0,0 +1,73 @@
+﻿
+#include <iostream>//содержит задание 4
+using namespace std;
+void Matr(int** matr, int n, int m) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matr[i][j] = rand()%20 - 10;
+        }
+    }
+}
+void printMatr(int** matr, int n, int m) {
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            cout << matr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+void transportationMain(int** matr, int n, int m)
+{
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < i; j++) {
+            int tranM = matr[i][j];
+            matr[i][j] = matr[j][i];
+            matr[j][i] = tranM;
+        }
+}
+void transportationSide(int** matr, int n, int m)
+{
+    for (int i = 0; i < (n - 1); i++) {
+ for (int j = 0; j < (n - 1) - i; j++)
+        {
+            int tranM = matr[i][j];
+            matr[i][j] = matr[(n - 1) - j][(n - 1) - i];
+            matr[(n - 1) - j][(n - 1) - i] = tranM;
+        }
+    }
+       
+}
+
+int main()
+{
+    int ** matr, n, m;
+    cout << "Input n(strings) ";
+    cin >> n;
+    cout << "Input m(collumns) ";
+    cin >> m;
+                 matr = new int*[n];
+    for (int i = 0; i < n; i++)
+        matr[i] = new int[n];
+    Matr(matr, n, m);
+    printMatr(matr, n, m);
+    transportationMain( matr, n, m);
+    cout << "Main-transportated matrix" << endl;
+    printMatr(matr, n, m);
+    transportationSide(matr, n, m);
+    cout << "side-transportated matrix" << endl;
+    printMatr(matr, n, m);
+    for (int i = 0; i < n; i++)
+    {
+        delete[] matr[i];
+    }
+
+    delete[] matr;
+    
+
+}
+
---
+++ С.Р._task_2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>//содержит задания 1, 2, 3
+using namespace std;
+void inputArr(int*  arr, int size)
+{
+	for (int i = 0; i < size; i++) 
+	{
+		arr[i] = rand() % 20 - 10;
+	}
+}
+void printArr(int* arr,int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << " " << arr[i];
+	}
+}
+void positNum(int* arr, int size, int num)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == num) 
+		{
+			cout << i;
+			i = size;
+		}
+	}
+}
+void searchDifference(int* arr, int size)
+{
+	for (int i = 2; i < size; i++)
+	{
+		if (arr[i] < (arr[i-1]- arr[i-2]))
+		{
+			cout << arr[i]<<" for position "<<i<<endl;
+			
+		}
+	}
+}
+int searchL(int* arr, int size)
+{
+	int number = 1;
+	int count = 1;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] == arr[i + 1]) count += 1;
+		else {
+			if (count > number) {
+				number = count;
+				count = 1;
+			}
+		}
+	}
+	return number;
+}
+int main()
+{
+	int num, lenght, size;
+	cout << "input array size " << endl;
+	cin >> size;
+	int* arr = new int[size];
+		inputArr(arr, size);
+		printArr(arr, size);
+		cout << "\ninput your number"<<endl;
+		cin >> num;
+		cout << "\nThe position of your number is ";
+		positNum(arr, size, num);
+		cout << "\nall numbers in the array that are less than the difference between the two previous ones:\n";
+		searchDifference(arr, size);
+		cout << "\nMax lenght is ";
+		lenght = searchL(arr, size);
+		cout << lenght;
+		delete[]arr;
+}
+
+
+
