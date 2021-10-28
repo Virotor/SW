420c4aa0cb4066a02e4b5479bf51658bc5aa218e
CannyDemon <ruskevich.ilya@mail.ru>
2021-10-28 20:58:57 +0300

    T1 init
    init:

---
+++ pz 28.10/Task 1/Task 1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    int* Arr;
+    int size,number;
+    cout << "Введите размер масива\n";
+    cin >> size;
+    Arr = new int[size]; // динамический массив
+    srand(time(NULL));
+    int num;
+    for (int i = 0; i < size; i++)
+    {
+        num = rand() % 20 + 1; // случайные числа от 0 до 20
+        Arr[i] = num;
+    }
+    for (int i = 0; i < size; i++) { cout << Arr[i] << " "; }
+    cout << endl;
+    cout << "Поиск номера\n";
+    cin >> number;
+    for (int i = 0; i < size; i++) {
+        if (Arr[i] == number) {
+            cout << "pos = " << i<< "\n";
+            i = size;
+        }
+        
+    }
+    delete[]Arr;
+
+    system("pause");
+}
