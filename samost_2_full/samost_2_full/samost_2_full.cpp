0862b47fe1111770ead3040c6b599b2be7dae222
San4ek <sankevich2003@mail.ru>
2021-10-28 19:34:08 +0300

    type: init  scope:T5
    short summary: task 5 completed and added

--- samost_2_full/samost_2_full/samost_2_full.cpp
+++ samost_2_full/samost_2_full/samost_2_full.cpp
@@ -131,3 +131,56 @@ void task4() {
 	}
 	cout << endl << "Task 4 completed" << endl << endl;
 }
+
+void task5() {
+	cout << "Task 5 start" << endl;
+	int n = enterNumber();
+	int xMin; cout << "Enter min coordinate x="; cin >> xMin;
+	int xMax; cout << "Enter max coordinate x="; cin >> xMax;
+	if (xMax < xMin) {
+		cout << "Error" << endl;
+		exit(0);
+	}
+
+	int yMin; cout << "Enter min coordinate y="; cin >> yMin;
+	int yMax; cout << "Enter max coordinate y="; cin >> yMax;
+	if (yMax < yMin) {
+		cout << "Error" << endl;
+		exit(0);
+	}
+
+	cout << endl;
+	cout << setw(10) << "n" << setw(10) << "X" << setw(10) << "Y" << endl;
+	double** coordinate = new double* [n];
+	for (int i = 0; i < n; i++) {
+		coordinate[i] = new double[2];
+		coordinate[i][0] = (rand() % (xMax - xMin + 1) + xMin) / double(rand() % (xMax - xMin + 1) + xMin);
+		cout << setw(10) << i << setw(10) << coordinate[i][0];
+		coordinate[i][1] = (rand() % (yMax - yMin + 1) + yMin) / double(rand() % (yMax - yMin + 1) + yMin);
+		cout << setw(10) << coordinate[i][0] << endl;
+	}
+	cout << endl;
+	double minLength = sqrt(pow(coordinate[0][0] - coordinate[1][0], 2) + pow(coordinate[0][1] - coordinate[1][1], 2));
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			double length = sqrt(pow(coordinate[i][0] - coordinate[j][0], 2) + pow(coordinate[i][1] - coordinate[j][1], 2));
+			if (length <= minLength) {
+				minLength = length;
+			}
+		}
+	}
+	cout << endl << "Min length=" << minLength << endl;
+	cout << "Task 5 completed" << endl;
+}
+
+int main() {
+	srand(time(0));
+	task1();
+	task2();
+	task3();
+	task4();
+	task5();
+
+	system("pause");
+	return 0;
+}
