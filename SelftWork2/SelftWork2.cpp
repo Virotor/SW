72e8c8874c03f24757946b3f3c117f01583c3b07
Franak <frangrandev@gmail.com>
2021-10-28 11:20:38 +0300

    feat: class Array

--- SelftWork2/SelftWork2.cpp
+++ SelftWork2/SelftWork2.cpp
@@ -1,6 +1,76 @@
 ﻿#include <iostream>
+#include <locale>
+#include <random>
+using namespace std;
+
+class Array
+{
+private:
+    int Size;
+    int* Arr;
+    void Clear()
+    {
+        if (Arr != nullptr)
+        {
+            delete[] Arr;
+        }
+    }
+    const int R_MAX = 40;
+    const int R_MIN = -20;
+public:
+    void Enter()
+    {
+        Clear();
+
+        cout << "Enter count of elements: ";
+        cin >> Size;
+        cout << "Fill massive: ";
+        Arr = new int[Size];
+        for (int i = 0; i < Size; i++)
+        {
+            cin >> Arr[i];
+        }
+    }
+    void RandEnter()
+    {
+        Clear();
+
+        cout << "Enter count of elements: ";
+        cin >> Size;
+        Arr = new int[Size];
+        for (int i = 0; i < Size; i++)
+        {
+            Arr[i] = R_MIN + rand() % (R_MAX);
+        }
+
+    }
+    void Print()
+    {
+        for (int i = 0; i < Size; i++)
+        {
+            cout << Arr[i] << " ";
+        }
+    }
+
+    
+
+    Array()
+    {
+        this->Size = 0;
+        Arr = new int[Size];
+    }
+    Array(int size)
+    {
+        this->Size = size;
+        Arr = new int[Size];
+    }
+    ~Array()
+    {
+        Clear();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
 }
