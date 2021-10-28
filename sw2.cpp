55be5ddd4df061fd0c39715cec81ac1c72fa5a7b
txrch <vladimir.iasienko@mail.ru>
2021-10-28 23:23:32 +0300

    main

---
+++ sw2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int ArrTwo(int*, int);
+void ArrThree(int*, int);
+void numfinder(int, int*, int);
+void LessNumber(int*, int);
+int Length(int*, int);
+
+
+void ArrOne(int*& x, int size)
+{
+	x = new int[size];
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+
+	int size, num;
+	cout << "Введите размер массива: "; cin >> size;
+
+	int* a;
+	ArrOne(a, size);
+	ArrTwo(a, size);
+	ArrThree(a, size);
+
+
+	cout << "/ / / / / Задание 1 / / / / /" << endl;
+	cout << endl << "Введите число: ";
+	cin >> num;
+	numfinder(num, a, size);
+
+	cout << "/ / / / / Задание 2 / / / / /" << endl;
+	LessNumber(a, size);
+
+	cout << "/ / / / / Задание 3 / / / / /" << endl;
+	cout << "Длина наибольшей последовательности одинаковых чисел: " << Length(a, size) << endl;
+	
+	return 0;
+}
+
+
+
+int ArrTwo(int* a, int size)
+{
+	for (int i = 0; i < size; i++)
+	
+		a[i] = rand() % 20 - 10;
+	
+	return *a;
+}
+
+void ArrThree(int* a, int size)
+{
+	for (int i = 0; i < size; i++)
+	
+		cout << a[i] << "  ";
+	
+	cout << endl;
+}
+
+
