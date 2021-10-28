4461880f6cfc2510205e7beaad1d20544a20ea7e
Shveder <89966688+Shveder@users.noreply.github.com>
2021-10-28 21:57:50 +0300

    Update Source.cpp
    Вторая часть не пашет

--- SW 2/ex 1/ex 1/Source.cpp
+++ SW 2/ex 1/ex 1/Source.cpp
@@ -5,9 +5,14 @@ int fillingArray(int*& a, int sizeOfArray);
 void deleteArray(int*&a);
 void printArray(int*& a, int sizeofArray);
 void findIndexOfCustomNumber(int*& a, int sizeOfArray);
+void makeArray2(char*& a, int sizeOfArray);
+char fillingArray2(char*& a, int sizeOfArray);
+void deleteArray2(char*& a);
+void findIndexOfCustomPartOfWorld(char*& a, int sizeOfArray);
 
 int main()
 {
+	srand(time(0));
 	int enteredNumber,*nameOfArray;
 	std::cout << "Input size of array:";
 	std::cin >> enteredNumber;
@@ -17,6 +22,16 @@ int main()
 	printArray(nameOfArray, enteredNumber);
 	findIndexOfCustomNumber(nameOfArray, enteredNumber);
 	deleteArray(nameOfArray);
+	std::cout << std::endl;
+	std::cout << "Additional task:" << std::endl;
+	std::cout << "Input size of text:" << std::endl;
+	int u;
+	char *p;
+	std::cin >> u;
+	makeArray2(p,u);
+	fillingArray2(p,u);
+	findIndexOfCustomPartOfWorld(p,u);
+	deleteArray2(p);
 	return 0;
 }
 void makeArray(int*& a, int sizeOfArray)
@@ -56,4 +71,49 @@ void findIndexOfCustomNumber(int*& a,int sizeOfArray)
 		if (i == sizeOfArray)
 			std::cout << "Invalid selection";
 	}
+}
+void makeArray2(char*& a, int sizeOfArray)
+{
+	a = new char[sizeOfArray];
+}
+char fillingArray2(char*& a, int sizeOfArray)
+{
+	for (int i = 0; i < sizeOfArray; i++)
+	{
+		scanf_s("%c", &a[i]);
+	}
+	return *a;
+}
+void deleteArray2(char*& a)
+{
+	delete[]a;
+}
+void findIndexOfCustomPartOfWorld(char*& a, int sizeOfArray)
+{
+	int letterNumber;
+	char* y;
+	std::cout << "Input number of letters"<<std::endl;
+	std::cin >> letterNumber;
+	makeArray2(y, letterNumber);
+	fillingArray2(y, letterNumber);
+	int ind = 0, s = 0,i;
+		for ( i = 0; i < sizeOfArray - letterNumber; i++)
+		{
+			for (int g = 0; g < letterNumber; g++)
+			{
+				if (a[i+g] == y[g])
+				{
+					s++;
+				}
+				s = 0;
+				if (s == letterNumber)
+				{
+					std::cout << "Index:" << ind << "-" << ind + letterNumber;
+					break;
+				}
+			}
+			ind++;
+	    }
+		if (i == sizeOfArray - letterNumber)
+			std::cout << "There is'nt this part of word here";
 }
