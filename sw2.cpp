0ed18cf1d24fe4a2c99bfc1cfdf24b65ca68b593
txrch <vladimir.iasienko@mail.ru>
2021-10-28 23:25:42 +0300

    task2

--- sw2.cpp
+++ sw2.cpp
@@ -77,3 +77,22 @@ void numfinder(int num, int* arr, int size)
 	cout << endl;
 }
 
+
+
+void LessNumber(int* a, int size)
+{
+	for (int i = 0; i < size - 2; i++)
+	{
+		if (a[i] - a[i + 1] > a[i + 2])
+
+			cout << a[i + 2] << endl;
+
+		else
+
+			if (i == size - 3)
+
+				cout << "Число не существует" << endl;
+
+
+	}
+}
