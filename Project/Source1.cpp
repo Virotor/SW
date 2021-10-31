2b79f91ce7576d0f7c7fe907f2336e4333b344e7
Anastasia <91217267+Anastasisassie@users.noreply.github.com>
2021-10-31 21:25:12 +0300

    feat (task): add task 2

--- Project/Source1.cpp
+++ Project/Source1.cpp
@@ -41,3 +41,40 @@ int main()
     system("pause");
     return 0;
 }
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+void numbers(double mass[], int size)
+{
+    for (int i = 0; i < size; i++)
+        if (mass[i] < mass[i - 1] - mass[i - 2])
+        {
+            cout << mass[i] << " ";
+
+        }
+
+}
+
+int main()
+{
+    system("color F0");
+    int size;
+    int A = -9;
+    int B = 9;
+    cout << "Enter the size of the array: "; cin >> size;
+    cout << "Array: ";
+    double* mass = new double[size];
+    srand(time(0));
+
+    for (int i = 0; i < size; i++)
+    {
+        mass[i] = A + rand() % ((B + 1) - A);
+        cout << setw(2) << mass[i] << " ";
+    }
+    cout << "Numbers ";
+    numbers(mass, size);
+    delete[]mass;
+    system("pause");
+    return 0;
+}
