fefe69d9acb890ea60b40da9efeb279529e6d409
Anastasia <91217267+Anastasisassie@users.noreply.github.com>
2021-10-31 21:26:57 +0300

    feat (task): add task 3

--- Project/Source1.cpp
+++ Project/Source1.cpp
@@ -78,3 +78,44 @@ int main()
     system("pause");
     return 0;
 }
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void findlength(int length[], int size, int k = 1, int max = 0)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (length[i] == length[i + 1])
+        {
+            k++;
+        }
+        else
+        {
+            k = 1;
+        }
+        if (k > max)
+            max = k;
+    }
+    cout << max << endl;
+}
+
+int main()
+{
+    system("color F0");
+    int size, i, k = 1, max = 0;
+    int A = -9;
+    int B = 9;
+    cout << "Enter size: "; cin >> size;
+    int* length = new int[size];
+    srand(time(0));
+    for (int i = 0; i < size; i++)
+    {
+        length[i] = A + rand() % ((B + 1) - A);
+        cout << setw(2) << length[i] << " ";
+
+    }
+    cout << "\nThe maximum length of repeated numbers: ";
+    findlength(length, size, k, max);
+    delete[] length;
+    return 0;
+}
