a2a74227d19d2a1e02d1e84a560b2c0d50657fab
ingarondel <90716651+ingarondel@users.noreply.github.com>
2021-12-08 16:58:03 +0300

    sw3

---
+++ SW3/SW3/Header.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct exam
+{
+	int mark;
+	int id;
+	string nameOfExam;
+	string averageMarkLabs;
+};
+
+exam* readExamFromFile(string path, int& size);
+exam* resizeArray(int& oldSize, int newSize, exam* exams);
+void writeExamInFile(std::string path, exam* exams, int size, int openMode);
+int sizeOfFile(std::string path);
+void numberOfLines(string path);
