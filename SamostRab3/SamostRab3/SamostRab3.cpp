0a4f43eb63414823c64bd24ef8503a9d7f2209f0
Vadim Tereshko <krolikrodger18895@gmail.com>
2021-12-08 17:10:09 +0300

    samrab3

---
+++ SamostRab3/SamostRab3/SamostRab3.cpp
@@ -0,0 +1,15 @@
+﻿#include "Class.h"
+
+#include <iostream>
+
+
+int main()
+{
+	int size = 1;
+	exam* exams = readExamFromFile("file.txt", size);
+	writeExamInFile("file1.txt", exams, size, std::ios_base::out, "forr");
+	std::cout << sizeOfFile("file1.txt") << " bites";
+	numberOfLines("file1.txt");
+
+	return 0;
+}
